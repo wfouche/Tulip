2c373cd4488a5a49b7fb5b92bc43be8c7cb38c1d
wfouche <werner.fouche@gmail.com>
2019-11-07 09:02:16 +0200

    Upgraded to Gradle 5.6.4

--- upgrade_gradle.cmd
+++ upgrade_gradle.cmd
@@ -1,4 +1,4 @@
 set JAVA_HOME="c:\Java\jdk-11.0.5+10"
 call .\gradlew --stop
 rd/q/s %USERPROFILE%\.gradle
-call .\gradlew wrapper --gradle-version=5.6.3
+call .\gradlew wrapper --gradle-version=5.6.4
