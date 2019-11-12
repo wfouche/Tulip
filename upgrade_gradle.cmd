bf02050feabc4f15688c92500b25e585215d6304
wfouche <werner.fouche@gmail.com>
2019-11-12 20:20:36 +0200

    Improved the upgrade script for Gradle.

--- upgrade_gradle.cmd
+++ upgrade_gradle.cmd
@@ -1,4 +1,12 @@
 set JAVA_HOME="c:\Java\jdk-11.0.5+10"
+
 call .\gradlew --stop
+
+timeout 5
 rd/q/s %USERPROFILE%\.gradle
+timeout 5
+rd/q/s .gradle
+timeout 5
+rd/q/s .idea
+
 call .\gradlew wrapper --gradle-version=5.6.4
