23c6e3bbd6f4202cc3a9000d896cdf3ba20d7980
wfouche <werner.fouche@gmail.com>
2019-12-01 03:54:56 -0500

    Switched to Asciidoctor 2.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@
 plugins {
 
     // Add support for AsciidoctorJ
-    id("org.asciidoctor.convert") version "2.3.0"
+    id("org.asciidoctor.convert") version "2.4.0"
 
     // Apply the Kotlin JVM plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.jvm") version "1.3.60"
