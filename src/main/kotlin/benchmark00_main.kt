e8410bff62cf9806b00621d6377ed35366acb832
wfouche <werner.fouche@gmail.com>
2020-11-11 11:28:41 +0200

    Revised to do list.

--- src/main/kotlin/benchmark00_main.kt
+++ src/main/kotlin/benchmark00_main.kt
@@ -14,4 +14,10 @@ fun main() {
 
 /*-------------------------------------------------------------------------*/
 
+// TODO: remove libs/okhttp, and add its as a Gradle dependency.
+
+// TODO: write an example REST call using okhttp.
+
 // TODO: write a UI front-end for Tulip with Jetpack Compose - https://www.infoworld.com/article/3596322/jetbrains-releases-desktop-ui-framework-for-kotlin.html
+
+/*-------------------------------------------------------------------------*/
