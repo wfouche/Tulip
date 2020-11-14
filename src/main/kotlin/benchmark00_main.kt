8932586e73473202279e0d3b9aa831014f3b8cea
wfouche <werner.fouche@gmail.com>
2020-11-14 15:31:20 +0200

    REST Service benchmark example.

--- src/main/kotlin/benchmark00_main.kt
+++ src/main/kotlin/benchmark00_main.kt
@@ -16,8 +16,6 @@ fun main() {
 
 // TODO: upgrade to Kotlin 1.4.20 from 1.4.20-RC
 
-// TODO: write documentation for Tulip.
-
 // TODO: write a UI front-end for Tulip with Jetpack Compose - https://www.infoworld.com/article/3596322/jetbrains-releases-desktop-ui-framework-for-kotlin.html
 
 /*-------------------------------------------------------------------------*/
