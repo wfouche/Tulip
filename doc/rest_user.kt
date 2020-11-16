abb556a91acdd89ec2c153a827798ece442569ef
wfouche <werner.fouche@gmail.com>
2020-11-16 19:36:00 +0200

    Code cleanup.

--- doc/rest_user.kt
+++ doc/rest_user.kt
@@ -10,25 +10,13 @@ class UserHttp(userId: Int) : User(userId) {
 
     val httpClient = OkHttpClient()
     val httpURL = "http://jsonplaceholder.typicode.com/photos/${userId+1}"
-
     val request = Request.Builder()
             .url(httpURL)
             .build()
-
-    override fun init(): Boolean {
-        Console.put("  $userId")
-        return true
-    }
 
     override fun action1(): Boolean {
         httpClient.newCall(request).execute().use { response ->
             return response.isSuccessful
         }
     }
-
-    override fun done(): Boolean {
-        Console.put("  $userId")
-        return true
-    }
-
 }
