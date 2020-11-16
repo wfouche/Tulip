55f6f243e0b86edf20b88a854a1774947ab87f84
wfouche <werner.fouche@gmail.com>
2020-11-16 22:25:53 +0200

    Update rest_user.kt

--- doc/rest_user.kt
+++ doc/rest_user.kt
@@ -7,13 +7,11 @@ import okhttp3.Request
 class UserHttp(userId: Int) : User(userId) {
 
     val httpClient = OkHttpClient()
-    val httpURL = "http://jsonplaceholder.typicode.com/photos/${userId+1}"
-    val request = Request.Builder()
-            .url(httpURL)
-            .build()
+    val httpURL1 = "http://jsonplaceholder.typicode.com/photos/${userId+1}"
+    val request1 = Request.Builder().url(httpURL).build()
 
     override fun action1(): Boolean {
-        httpClient.newCall(request).execute().use { response ->
+        httpClient.newCall(request1).execute().use { response ->
             return response.isSuccessful
         }
     }
