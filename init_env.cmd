0c90e4ad12c6f07cbd11d0e98d33331a65444b77
wfouche <werner.fouche@gmail.com>
2020-11-24 11:24:25 +0200

    Disabled command-line Kotlin compiler.

--- init_env.cmd
+++ init_env.cmd
@@ -2,4 +2,4 @@ REM JAVA_HOME="C:\Java\jdk8u272-b10"
 set JAVA_HOME="C:\Java\jdk-11.0.9+11"
 REM JAVA_HOME="C:\Java\jdk-15.0.1+9"
 
-set KOTLIN_HOME=C:\Kotlin\kotlin-compiler-1.4.10\kotlinc
+REM set KOTLIN_HOME=C:\Kotlin\kotlin-compiler-1.4.20\kotlinc
