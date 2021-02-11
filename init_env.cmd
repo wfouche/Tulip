c7c0338d9831f885f5016a0d1bd781c208369404
wfouche <werner.fouche@gmail.com>
2021-02-11 17:17:11 +0200

    Upgraded to JDK 11.0.10

--- init_env.cmd
+++ init_env.cmd
@@ -1,5 +1,5 @@
 REM JAVA_HOME="C:\Java\jdk8u272-b10"
-set JAVA_HOME="C:\Java\jdk-11.0.9+11"
+set JAVA_HOME="C:\Java\jdk-11.0.10+9"
 REM JAVA_HOME="C:\Java\jdk-15.0.1+9"
 
 REM set KOTLIN_HOME=C:\Kotlin\kotlin-compiler-1.4.20\kotlinc
