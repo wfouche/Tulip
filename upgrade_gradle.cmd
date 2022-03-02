7d0e9b6e3d4b9393fdc7a8706f2ee9c6f34211a3
wfouche <werner.fouche@gmail.com>
2022-03-02 09:46:31 +0200

    Upgraded to Kotlinn 1.6.20-RC and Gradpe 7.4

--- upgrade_gradle.cmd
+++ upgrade_gradle.cmd
@@ -1,2 +1,2 @@
 call init_env.cmd
-gradlew.bat wrapper --gradle-version=7.3.3
+gradlew.bat wrapper --gradle-version=7.4
--- upgrade_gradle.sh
+++ upgrade_gradle.sh
@@ -1,1 +1,1 @@
-./gradlew wrapper --gradle-version=7.3.3
+./gradlew wrapper --gradle-version=7.4
