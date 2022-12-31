d8f2e5595e09cc2fa92f4ca684574b4ac29e04c8
wfouche <werner.fouche@gmail.com>
2022-12-31 10:30:23 +0200

    Upgraded Kotlin to 1.8.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
     id("org.asciidoctor.jvm.convert") version "3.3.2"
 
     // Apply the Kotlin JVM plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.8.0-RC"
+    id("org.jetbrains.kotlin.jvm") version "1.8.0"
 
     // Apply the application plugin to add support for building a CLI application.
     application
