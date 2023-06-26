a8ec9e68d2c17767f4de93d4cfc3531177f8258e
wfouche <werner.fouche@gmail.com>
2023-06-26 16:54:34 +0200

    Display JVM memory usage

--- src/main/kotlin/tulip/tulip_stats.kt
+++ src/main/kotlin/tulip/tulip_stats.kt
@@ -225,9 +225,19 @@ class ActionStats {
         )
 
         if (action_id == NUM_ACTIONS) {
+            val fm = Runtime.getRuntime().freeMemory()
+            val tm = Runtime.getRuntime().totalMemory()
+            val mm = Runtime.getRuntime().maxMemory()
+
             output.add("")
             output.add("  average cpu load (process) = ${"%.3f".format(Locale.US, r.avg_cpu_process)}")
             output.add("  average cpu load (system ) = ${"%.3f".format(Locale.US, r.avg_cpu_system)}")
+
+            output.add("")
+            output.add("  JVM used memory    = ${"%,d".format(Locale.US, tm-fm)}")
+            output.add("  JVM free memory    = ${"%,d".format(Locale.US, fm)}")
+            output.add("  JVM total memory   = ${"%,d".format(Locale.US, tm)}")
+            output.add("  JVM maximum memory = ${"%,d".format(Locale.US, mm)}")
         }
 
         Console.put(output)
