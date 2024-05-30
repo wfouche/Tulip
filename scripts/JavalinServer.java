8474ea18594496f7eb349041d0f56356be3f1b7a
Werner Fouché <werner.fouche@gmail.com>
2024-05-30 10:50:07 +0200

    Javalin example REST service

---
+++ scripts/JavalinServer.java
@@ -0,0 +1,13 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+//DEPS io.javalin:javalin:6.1.6
+//DEPS org.slf4j:slf4j-simple:2.0.12
+
+import io.javalin.Javalin;
+
+public class JavalinServer {
+    public static void main(String[] args) {
+        var app = Javalin.create(/*config*/)
+            .get("/", ctx -> ctx.result("Hello World"))
+            .start(7070);
+    }
+}
---
+++ scripts/run_JavalinServer.sh
@@ -0,0 +1,7 @@
+# Shell 1 - run JavalinServer
+jbang JavalinServer.java
+
+# Shell 2 - benchmark JavalinServer with Apache Bench
+# ab -c 4 -t 5 http://localhost:7070/hello
+
+# Shell 3 - benchmark JavalinServer with Tulip
