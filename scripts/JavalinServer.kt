3cd43df27eee2e006ad6ab4215265aee32508b41
Werner Fouché <werner.fouche@gmail.com>
2024-05-30 12:13:15 +0200

    Fixed Kotlin Script warnings

--- scripts/JavalinServer.kt
+++ scripts/JavalinServer.kt
@@ -1,6 +1,6 @@
-#!/usr/bin/env kscript
-//DEPS io.javalin:javalin:6.1.6
-//DEPS org.slf4j:slf4j-simple:2.0.12
+@file:Repository("https://repo.maven.apache.org/maven2")
+@file:DependsOn("io.javalin:javalin:6.1.6")
+@file:DependsOn("org.slf4j:slf4j-simple:2.0.12")
 
 // https://javalin.io/
 import io.javalin.Javalin
