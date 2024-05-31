eaaab4b94bd289c791045227d4a16f59dc1538aa
Werner Fouché <werner.fouche@gmail.com>
2024-05-31 10:41:44 +0200

    Start of benchmark results analysis functionality

---
+++ src/tulip/tulip_results.kt
@@ -0,0 +1,6 @@
+package tulip
+
+import com.google.gson.annotations.SerializedName
+import com.google.gson.GsonBuilder
+
+// This is a placeholder for results analysis and summary at the end of a benchmark run.
