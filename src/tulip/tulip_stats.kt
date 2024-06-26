5ea28e808b2ca8489d15cef2a2fae85f10236b72
Werner Fouché <werner.fouche@gmail.com>
2024-06-26 12:51:21 +0200

    Resolved the coordination omission problem issue
    https://redhatperf.github.io/post/coordinated-omission/

--- src/tulip/tulip_stats.kt
+++ src/tulip/tulip_stats.kt
@@ -304,7 +304,7 @@ class ActionStats {
         // key = response time in microseconds (NOT milliseconds).
         // value = the number of times a given (key) response time occurred.
 
-        val durationMicros = task.serviceTimeMicros + task.endQueueTimeNanos - task.beginQueueTimeNanos
+        val durationMicros = task.serviceTimeMicros + (task.endQueueTimeNanos - task.beginQueueTimeNanos)/1000
         val key = llq(durationMicros)
 
         // Attempt 1:
