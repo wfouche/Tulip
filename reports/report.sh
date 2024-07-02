4d99f6472e294d3aad2ec163e973658694f1855f
Werner Fouché <werner.fouche@gmail.com>
2024-07-02 13:17:48 +0200

    Additional reporting testing and cleanup

--- reports/report.sh
+++ reports/report.sh
@@ -1,1 +1,1 @@
-java -Dpython.path=./jars/HdrHistogram-2.2.2.jar -jar ./jars/jython-standalone-2.7.3.jar  report.py ./tulip_results.json > report.html
+java -Dpython.path=./jars/HdrHistogram-2.2.2.jar -jar ./jars/jython-standalone-2.7.3.jar  report.py ../tulip_results.json > report.html
