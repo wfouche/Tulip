1a43a9e50c3daf8a564f92020feda355ee1e7e8f
Werner Fouché <werner.fouche@gmail.com>
2024-07-02 11:23:34 +0200

    Moved Kotlin scripting support to JBang
    Not using kscript anymore. It does not work with Kotlin 2.0.0.

---
+++ scripts/JavalinServer.kt
@@ -0,0 +1,18 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+//DEPS io.javalin:javalin:6.1.6
+//DEPS org.slf4j:slf4j-simple:2.0.12
+
+// https://javalin.io/
+import io.javalin.Javalin
+
+public fun main() {
+    val app = Javalin.create(/*config*/)
+        .get("/posts/{id}") { ctx -> ctx.result("{\"code\": \"OK\"}").contentType("application/json") }
+        .get("/comments/{id}") { ctx -> ctx.result("{\"code\": \"OK\"}").contentType("application/json") }
+        .get("/albums/{id}") { ctx -> ctx.result("{\"code\": \"OK\"}").contentType("application/json") }
+        .get("/photos/{id}") { ctx -> ctx.result("{\"code\": \"OK\"}").contentType("application/json") }
+        .get("/todos/{id}") { ctx -> ctx.result("{\"code\": \"OK\"}").contentType("application/json") }
+        .start(7070)
+}
+// $ ./JavalinServer.kt        # or use command below
+// $ jbang JavalinServer.kt
