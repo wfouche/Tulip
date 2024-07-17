09f78e15a819e3404d6185a1a9e424bf54d7e43a
Werner Fouché <werner.fouche@gmail.com>
2024-07-17 20:02:51 +0200

    Optimized GC and Memory settings for test suite

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,30 @@
 application {
     // Java 21 - https://docs.oracle.com/en/java/javase/21/gctuning/z-garbage-collector.html
     applicationDefaultJvmArgs = listOf(
+
         "-server",
-        "-Xmx4096m",
+
+        // Create a 2 GB regions of Large Pages
+        //
+        // $ ls /sys/kernel/mm/hugepages/
+        //   hugepages-1048576kB  hugepages-2048kB
+        //
+	    // $ echo 2048 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
+        //
+	    // "-XX:+UseLargePages",
+
+        "-Xms2048m",
+	    "-Xmx2048m",
+
+        "-XX:+UseParallelGC",
         // "-XX:+UseG1GC", "-XX:+UseDynamicNumberOfGCThreads",
-        "-XX:+UseZGC", "-XX:+ZGenerational", "-XX:+UseDynamicNumberOfGCThreads",
-        "-Dcom.sun.management.jmxremote.port=3333",
-        "-Dcom.sun.management.jmxremote.ssl=false",
-        "-Dcom.sun.management.jmxremote.authenticate=false")
+        // "-XX:+UseZGC", "-XX:+ZGenerational", "-XX:+UseDynamicNumberOfGCThreads",
+
+        // "-Dcom.sun.management.jmxremote.port=3333",
+        // "-Dcom.sun.management.jmxremote.ssl=false",
+        // "-Dcom.sun.management.jmxremote.authenticate=false"
+
+        )
 
     // Define the main class for the application
     mainClass.set("benchmark00")
