9be11452cd7ce76e609659effac91a01392412ed
Werner Fouché <werner.fouche@gmail.com>
2024-07-29 13:23:49 +0200

    Added CLI parameter parsing to Benchmark01

--- src/Benchmark01.java
+++ src/Benchmark01.java
@@ -1,11 +1,23 @@
 import tulip.api.TulipApi;
-import tulip.api.TulipUserFactory;
-import tulip.user.TulipUser;
+import org.apache.commons.cli.*;
 
 public class Benchmark01 {
 
     public static void main(String[] args) {
-        TulipApi.runTulip("./src/user/http/config.json", new Benchmark01UserFactory());
+        Options options = new Options();
+        options.addOption("c", "config", true, "Benchmark configuration file");
+
+        CommandLineParser parser = new DefaultParser();
+        try {
+            CommandLine cmd = parser.parse(options, args);
+            String configFile = cmd.getOptionValue("config");
+            TulipApi.runTulip(configFile, new Benchmark01UserFactory());
+
+        } catch (ParseException e) {
+            System.err.println("Error: " + e.getMessage());
+            System.exit(1);
+        }
+
     }
 
 }
