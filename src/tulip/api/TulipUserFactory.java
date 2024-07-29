2e978dfa52700670cd0b53cf0560f97c542afdbe
Werner Fouché <werner.fouche@gmail.com>
2024-07-29 13:28:04 +0200

    Added TODO to implement dynamic class loading (not urgent)

--- src/tulip/api/TulipUserFactory.java
+++ src/tulip/api/TulipUserFactory.java
@@ -5,6 +5,7 @@ import tulip.user.TulipUser;
 public abstract class TulipUserFactory {
 
     public TulipUser getUser(int userId, String className) {
+        // TODO - implement dynamic class loading (not urgent)
         return null;
     }
 
