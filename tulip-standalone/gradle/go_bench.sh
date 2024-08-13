68c04b82d4b3a7e843e42b810858af0d73a81e0a
Werner Fouché <werner.fouche@gmail.com>
2024-08-13 22:13:36 +0200

    Publish tulip-runtime JAR before running standalone benchmarks

--- tulip-standalone/gradle/go_bench.sh
+++ tulip-standalone/gradle/go_bench.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+pushd ../../
+./publish.sh
+popd
+
 ../../gradlew run --args="--config benchmark_config.json"
 
 ./report.sh
--- tulip-standalone/jbang/go_bench.sh
+++ tulip-standalone/jbang/go_bench.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+pushd ../../
+./publish.sh
+popd
+
 export JBANG_JAVA_OPTIONS="-server -Xms2048m -Xmx2048m -XX:+UseZGC -XX:+ZGenerational -XX:+UseDynamicNumberOfGCThreads"
 
 jbang run main.kt --config=benchmark_config.json
