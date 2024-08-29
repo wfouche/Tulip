2e50fea5029cb2284091a454dc3dabfdeaaba902
Werner Fouché <werner.fouche@gmail.com>
2024-08-29 15:26:08 +0200

    Removed String return type

--- tulip-runtime/src/org/tulip/api/TulipApi.java
+++ tulip-runtime/src/org/tulip/api/TulipApi.java
@@ -5,11 +5,11 @@ import org.tulip.report.TulipReportKt;
 
 public class TulipApi {
 
-    public static final String VERSION_STRING = "2.0.0-beta6";
+    public static final String VERSION_STRING = "2.0.0-beta7";
 
     public static final int NUM_ACTIONS = 100;
 
-    public static String runTulip(String configFilename, TulipUserFactory userFactory) {
+    public static void runTulip(String configFilename, TulipUserFactory userFactory) {
         String outputFilename = TulipKt.initConfig(configFilename);
         TulipKt.runTests(userFactory);
         TulipReportKt.createHtmlReport(outputFilename);
