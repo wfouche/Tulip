a3c7c23937adaa1dc98a6ae1917643a2424a7d0c
Werner Fouché <werner.fouche@gmail.com>
2024-08-29 17:45:09 +0200

    go_bench.cmd - use new embedded reporting facility

--- tulip-standalone/gradle/clean.sh
+++ tulip-standalone/gradle/clean.sh
@@ -1,1 +1,1 @@
-rm -f benchmark_output.json benchmark_report.html
+rm -f -r benchmark_output.json benchmark_report.html build
--- tulip-standalone/gradle/report.sh
+++
@@ -1,3 +0,0 @@
-java -Dpython.path=./report-jars/HdrHistogram-2.2.2.jar -jar ./report-jars/jython-standalone-2.7.3.jar report.py benchmark_output.json 1
-echo ""
-w3m -dump -cols 200 benchmark_report.html
