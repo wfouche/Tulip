23a9029874967ce071972b237b745a3a58e7258d
Werner Fouché <werner.fouche@gmail.com>
2024-09-12 18:28:45 +0200

    jbang project clean up

--- tulip-standalone/jbang/go_bench.sh
+++ tulip-standalone/jbang/go_bench.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-pushd ../../
-./publish.sh
-popd
-
 export JBANG_JAVA_OPTIONS="-server -Xms2048m -Xmx2048m -XX:+UseZGC -XX:+ZGenerational -XX:+UseDynamicNumberOfGCThreads"
 
 jbang run main.kt --config=benchmark_config.jsonc
