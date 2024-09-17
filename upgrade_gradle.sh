1fd6494fbcc0f623cd58645d7ee297a3a8509b0a
Werner Fouché <werner.fouche@gmail.com>
2024-09-17 19:04:31 +0200

    Downgraded Gradle for standalone benchmarks to 8.6

--- upgrade_gradle.sh
+++ upgrade_gradle.sh
@@ -1,2 +1,15 @@
 # ./gradlew wrapper --gradle-version latest
-./gradlew wrapper --gradle-version 8.6
+#
+# https://github.com/JetBrains/amper/blob/release/0.4/docs/Usage.md
+#
+GRADLE_VERSION=8.6
+
+./gradlew wrapper --gradle-version $GRADLE_VERSION
+
+pushd tulip-standalone/gradle
+./gradlew wrapper --gradle-version $GRADLE_VERSION
+popd
+
+pushd tulip-standalone/oppwa
+./gradlew wrapper --gradle-version $GRADLE_VERSION
+popd
