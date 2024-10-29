b7382ffded018ee34a75d6b7f0e125ef3c9d1bec
Werner Fouché <werner.fouche@gmail.com>
2024-10-29 12:40:39 +0200

    Support transparent loading of user classes

--- tulip-runtime/src/main/java/io/github/wfouche/tulip/api/TulipUserFactory.java
+++ tulip-runtime/src/main/java/io/github/wfouche/tulip/api/TulipUserFactory.java
@@ -1,10 +1,24 @@
 package io.github.wfouche.tulip.api;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class TulipUserFactory {
 
     public TulipUser getUser(int userId, String className, int threadId) {
-        // TODO - implement dynamic class loading
-        return null;
+        try {
+            Class<?> loadedClass = Class.forName(className);
+            Constructor<?> ctor=loadedClass.getConstructor(int.class, int.class);
+            return (TulipUser) ctor.newInstance(userId, threadId);
+        } catch (ClassNotFoundException | NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
