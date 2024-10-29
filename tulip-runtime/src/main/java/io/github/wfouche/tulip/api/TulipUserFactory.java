b1bdec3f0842a4a04ac94bc4ab9dec0a4aafd2ba
Werner Fouché <werner.fouche@gmail.com>
2024-10-29 10:22:26 +0200

    TulipUserFactory should not be an abstract class

--- tulip-runtime/src/main/java/io/github/wfouche/tulip/api/TulipUserFactory.java
+++ tulip-runtime/src/main/java/io/github/wfouche/tulip/api/TulipUserFactory.java
@@ -1,6 +1,6 @@
 package io.github.wfouche.tulip.api;
 
-public abstract class TulipUserFactory {
+public class TulipUserFactory {
 
     public TulipUser getUser(int userId, String className, int threadId) {
         // TODO - implement dynamic class loading
