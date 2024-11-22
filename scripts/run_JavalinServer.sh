162a3221defecaf4c79f462cfa84c01892be4e96
Werner Fouché <werner.fouche@gmail.com>
2024-11-22 09:20:42 +0200

    Added JBANG_JAVA_OPTIONS

--- scripts/run_JavalinServer.sh
+++ scripts/run_JavalinServer.sh
@@ -1,4 +1,7 @@
 # Shell 1 - run JavalinServer
+#
+export JBANG_JAVA_OPTIONS="-server -Xms2048m -Xmx2048m -XX:+UseZGC -XX:+ZGenerational -XX:+UseDynamicNumberOfGCThreads"
+
 jbang JavalinServer.java
 
 # Shell 2 - benchmark JavalinServer with Apache Bench
