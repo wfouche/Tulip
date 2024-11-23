374f860b093312cb0192f3be04c0deb9ddbbcdb1
Werner Fouché <werner.fouche@gmail.com>
2024-11-23 21:34:58 +0200

    Use instance main method

--- tulip-standalone/mbench-jbang-java/App.java
+++ tulip-standalone/mbench-jbang-java/App.java
@@ -2,19 +2,9 @@
 //DEPS io.github.wfouche.tulip:tulip-runtime:0.1.0-SNAPSHOT
 //SOURCES HttpUser.java
 
-/*-------------------------------------------------------------------------*/
-
 import io.github.wfouche.tulip.api.TulipApi;
 import io.github.wfouche.tulip.api.TulipUserFactory;
 
-/*-------------------------------------------------------------------------*/
-
-public class App {
-
-    public static void main(String[] args) {
-        TulipApi.runTulip("./benchmark_config.json", new TulipUserFactory());
-    }
-
+void main() {
+   TulipApi.runTulip("./benchmark_config.json", new TulipUserFactory());
 }
-
-/*-------------------------------------------------------------------------*/
--- tulip-standalone/mbench-jbang-java/run_bench.sh
+++ tulip-standalone/mbench-jbang-java/run_bench.sh
@@ -2,7 +2,8 @@
 
 export JBANG_JAVA_OPTIONS="-server -Xms2048m -Xmx2048m -XX:+UseZGC -XX:+ZGenerational -XX:+UseDynamicNumberOfGCThreads"
 
-jbang run App.java
+# https://www.infoq.com/news/2023/06/jbang-107/
+jbang --java 21 --enable-preview App.java
 
 echo ""
 #lynx -dump -width 200 benchmark_report.html
