1ced418378a14e27901e61dae515505545a72763
Werner Fouché <werner.fouche@gmail.com>
2024-11-25 21:30:00 +0200

    Test script

---
+++ publishToMavenCentral.sh
@@ -0,0 +1,7 @@
+# https://jreleaser.org/guide/latest/examples/maven/maven-central.html#_gradle
+
+./gradlew :tulip-runtime:publishToMavenLocal
+
+./gradlew jreleaserPublish
+
+# ./gradlew jreleaserFullRelease
