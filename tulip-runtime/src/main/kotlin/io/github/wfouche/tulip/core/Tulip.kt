eac6cb56984b1cfb6184914efea1b5e4a7b6be05
Werner Fouché <werner.fouche@gmail.com>
2024-11-26 11:28:05 +0200

    Renamed benchmark_duration_repeat_count to benchmark_repeat_count

--- tulip-runtime/src/main/kotlin/io/github/wfouche/tulip/core/Tulip.kt
+++ tulip-runtime/src/main/kotlin/io/github/wfouche/tulip/core/Tulip.kt
@@ -301,7 +301,7 @@ data class ConfigDuration(
     @SerialName("prewarmup_duration") val startupDuration: Long = 0,
     @SerialName("warmup_duration") val warmupDuration: Long = 0,
     @SerialName("benchmark_duration") val mainDuration: Long = 0,
-    @SerialName("benchmark_duration_repeat_count") val mainDurationRepeatCount: Int = 1
+    @SerialName("benchmark_repeat_count") val mainDurationRepeatCount: Int = 1
 )
 
 @Serializable
