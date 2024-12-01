e7abf5ad45ea80215a0bc2d9d0b929abd74d701d
Werner Fouché <werner.fouche@gmail.com>
2024-12-01 20:18:19 +0200

    Removed test script

--- publishToMavenCentral.sh
+++
@@ -1,7 +0,0 @@
-# https://jreleaser.org/guide/latest/examples/maven/maven-central.html#_gradle
-
-./gradlew :tulip-runtime:publishToMavenLocal
-
-./gradlew jreleaserPublish
-
-# ./gradlew jreleaserFullRelease
