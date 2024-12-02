fba54690213749b8322803328c31e7c2c450a2e1
Werner Fouché <werner.fouche@gmail.com>
2024-12-02 21:54:11 +0200

    Update script to sign artifacts and create ZIP file

--- 00-maven-test-publish.cmd
+++ 00-maven-test-publish.cmd
@@ -1,5 +1,28 @@
+REM Use this script to create the ZIP file to be uploaded to Maven Central
+REM https://central.sonatype.com/
+REM
+set tver=2.0.0
+
+REM Remove staging folder
 rd/q/s .\tulip-runtime\build\staging-deploy
 
+REM Publish JARs to staging folder
 call .\gradlew.bat publish
 
-dir /s .\tulip-runtime\build\staging-deploy
+REM Sign the artifacts
+pushd .\tulip-runtime\build\staging-deploy\io\github\wfouche\tulip\tulip-runtime\%tver%
+gpg -ab tulip-runtime-%tver%-javadoc.jar
+gpg -ab tulip-runtime-%tver%-sources.jar
+gpg -ab tulip-runtime-%tver%.jar
+gpg -ab tulip-runtime-%tver%.module
+gpg -ab tulip-runtime-%tver%.pom
+popd
+
+REM ZIP the files to be uploaded to Maven Central
+pushd .\tulip-runtime\build\staging-deploy
+zip -r tulip-runtime-%tver%.zip io
+popd
+
+REM Display artifact files and ZIP file
+dir /s .\tulip-runtime\build\staging-deploy\io\github\wfouche\tulip\tulip-runtime\%tver%
+dir   .\tulip-runtime\build\staging-deploy\*.zip
