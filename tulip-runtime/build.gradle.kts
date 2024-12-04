08c1d31a6d132c91776813e32d5833bd3ab4504a
Werner Fouché <werner.fouche@gmail.com>
2024-12-04 11:55:50 +0200

    Disable jreleaser in Gradle build file

--- tulip-runtime/build.gradle.kts
+++ tulip-runtime/build.gradle.kts
@@ -82,29 +82,29 @@ publishing {
     }
 }
 
-jreleaser {
-    project {
-        copyright.set("Werner Fouché")
-    }
-    gitRootSearch.set(true)
-    signing {
-        active.set(Active.ALWAYS)
-        armored.set(true)
-    }
-    deploy {
-        maven {
-            nexus2 {
-                create("maven-central") {
-                    active.set(Active.ALWAYS)
-                    url.set("https://s01.oss.sonatype.org/service/local")
-                    closeRepository.set(true)
-                    releaseRepository.set(true)
-                    stagingRepositories.add("build/staging-deploy")
-                }
-            }
-        }
-    }
-}
+//jreleaser {
+//    project {
+//        copyright.set("Werner Fouché")
+//    }
+//    gitRootSearch.set(true)
+//    signing {
+//        active.set(Active.ALWAYS)
+//        armored.set(true)
+//    }
+//    deploy {
+//        maven {
+//            nexus2 {
+//                create("maven-central") {
+//                    active.set(Active.ALWAYS)
+//                    url.set("https://s01.oss.sonatype.org/service/local")
+//                    closeRepository.set(true)
+//                    releaseRepository.set(true)
+//                    stagingRepositories.add("build/staging-deploy")
+//                }
+//            }
+//        }
+//    }
+//}
 
 tasks.withType<DokkaTask>().configureEach {
     dokkaSourceSets {
