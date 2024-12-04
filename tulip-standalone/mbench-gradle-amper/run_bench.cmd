0f5d0fcd93ef5a5638404a78e05793570ac908c0
Werner Fouché <werner.fouche@gmail.com>
2024-12-04 12:19:12 +0200

    Enabled -q option when using Gradle on Windows

--- tulip-standalone/mbench-gradle-amper/run_bench.cmd
+++ tulip-standalone/mbench-gradle-amper/run_bench.cmd
@@ -1,4 +1,4 @@
-call .\gradlew.bat  run --args="--config benchmark_config.json"
+call .\gradlew.bat -q run --args="--config benchmark_config.json"
 @echo off
 echo.
 ..\..\platform\msys64\bin\w3m.exe -dump -cols 200 benchmark_report.html
