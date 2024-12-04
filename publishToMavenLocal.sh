67b5caa6183fd8251e3382d71a1e885dc75bc00d
Werner Fouché <werner.fouche@gmail.com>
2024-12-04 23:33:40 +0200

    Sort local Maven filenames

--- publishToMavenLocal.sh
+++ publishToMavenLocal.sh
@@ -2,7 +2,7 @@
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk env
 
-# Publish tulip-runtime-1.0.0-SNAPSHOT.jar to local Maven
+# Publish tulip-runtime.jar to local Maven
 ./gradlew :tulip-runtime:publishToMavenLocal
 
-find ~/.m2 -name 'tulip-runtime*.jar' -print
+find ~/.m2 -name 'tulip-runtime*.jar' -print | sort
