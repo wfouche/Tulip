1f74fbeed9c50f33e52625b6b7f4916ed5bbbcd0
Werner Fouché <werner.fouche@gmail.com>
2024-12-05 22:56:13 +0200

    Simplified App.kt

--- tulip-standalone/demo-jbang/App.kt
+++ tulip-standalone/demo-jbang/App.kt
@@ -1,21 +1,12 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS com.github.ajalt.clikt:clikt-jvm:5.0.1
 //DEPS io.github.wfouche.tulip:tulip-runtime:2.0.0
 //JAVA 21
 //SOURCES DemoUser.kt
 
-import com.github.ajalt.clikt.core.*
-import com.github.ajalt.clikt.parameters.options.default
-import com.github.ajalt.clikt.parameters.options.option
+import io.github.wfouche.tulip.api.*
 
-import io.github.wfouche.tulip.api.TulipApi
-import io.github.wfouche.tulip.api.TulipUserFactory
-
-class TulipCli : CliktCommand() {
-    private val configOpt by option("--config").default("benchmark_config.json")
-    override fun run() {
-        TulipApi.runTulip(configOpt, TulipUserFactory())
-    }
+fun main(args: Array<String>) {
+    TulipApi.runTulip(
+        "benchmark_config.json",
+        TulipUserFactory())
 }
-
-fun main(args: Array<String>) = TulipCli().main(args)
