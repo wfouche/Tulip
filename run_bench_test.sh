34586494aadfc216fb91f2db3cb52acb18ad0d49
Werner Fouché <werner.fouche@gmail.com>
2024-12-05 19:52:45 +0200

    Made shell script executable

--- run_bench_test.sh
+++ run_bench_test.sh
@@ -1,3 +1,3 @@
-# call .\gradlew :tulip-app:run --args="--config benchmark_config_test.json"
-# call .\test_report.cmd
+# ./gradlew :tulip-app:run --args="--config benchmark_config_test.json"
+# ./test_report.sh
 ./build_and_run.sh --args="--config benchmark_config_test.json"
