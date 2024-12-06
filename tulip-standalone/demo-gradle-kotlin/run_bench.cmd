a5e6efc7d46caddec587a3bf511cfff2d75f37a0
Werner Fouché <werner.fouche@gmail.com>
2024-12-06 18:39:23 +0200

    Demo projects cleanup

--- tulip-standalone/demo-gradle-kotlin/run_bench.cmd
+++
@@ -1,4 +0,0 @@
-call .\gradlew -q run --args="--config benchmark_config.json"
-@echo off
-echo.
-..\..\platform\msys64\bin\w3m.exe -dump -cols 200 app/benchmark_report.html
--- tulip-standalone/demo-gradle-kotlin/run_bench.sh
+++
@@ -1,3 +0,0 @@
-./gradlew run --args="--config benchmark_config.json"
-echo ""
-lynx -dump -width 200 app/benchmark_report.html
