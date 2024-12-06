388bc8b99bb25ed6db245a1136329c9ec4d86112
Werner Fouché <werner.fouche@gmail.com>
2024-12-06 19:07:10 +0200

    Implement TulipConsole.put(List<String) (#23)
    Close #23

--- tulip-runtime/src/main/java/io/github/wfouche/tulip/api/TulipConsole.java
+++ tulip-runtime/src/main/java/io/github/wfouche/tulip/api/TulipConsole.java
@@ -2,6 +2,8 @@ package io.github.wfouche.tulip.api;
 
 import io.github.wfouche.tulip.core.Console;
 
+import java.util.List;
+
 /**
  * API class to access Tulip's Console object.
  */
@@ -21,5 +23,16 @@ public class TulipConsole {
      */
     public static void put (String text) {
         Console.put(text);
+    }
+
+    /**
+     * Outputs a list of strings to the Tulip console.
+     * This method serves as a wrapper for the Console.put() method,
+     * providing a simplified interface for console output in the Tulip framework.
+     *
+     * @param list The string to be output to the console.
+     */
+    public static void put (List<String> list) {
+        Console.put(list);
     }
 }
