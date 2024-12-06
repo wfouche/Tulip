78071765616487c57cbe2b846b0cec6937e02f86
Werner Fouché <werner.fouche@gmail.com>
2024-12-06 18:32:42 +0200

    Added Gradle versions plugin to demo benchmarks

--- tulip-app/build.gradle.kts
+++ tulip-app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 dependencies {
-    implementation("com.github.ajalt.clikt:clikt-jvm:5.0.1")
+    implementation("com.github.ajalt.clikt:clikt-jvm:5.0.2")
     implementation(project(":tulip-runtime"))
 }
 
--- tulip-standalone/demo-gradle-kotlin/app/build.gradle.kts
+++ tulip-standalone/demo-gradle-kotlin/app/build.gradle.kts
@@ -11,6 +11,8 @@ plugins {
 
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+
+    id("com.github.ben-manes.versions") version "0.51.0"
 }
 
 repositories {
