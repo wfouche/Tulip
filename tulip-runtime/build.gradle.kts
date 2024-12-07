e88cc248bd7e666a7425a52304ccb320596f887b
Werner Fouché <werner.fouche@gmail.com>
2024-12-07 09:05:18 +0200

    Set version to 2.0.1

--- tulip-runtime/build.gradle.kts
+++ tulip-runtime/build.gradle.kts
@@ -5,7 +5,7 @@ import org.jreleaser.model.Active
 import org.jetbrains.dokka.gradle.DokkaTask
 
 group = "io.github.wfouche.tulip"
-version = "2.0.1-dev"
+version = "2.0.1"
 
 plugins {
     id("com.github.ben-manes.versions") version "0.51.0"
--- tulip-runtime/src/main/java/io/github/wfouche/tulip/api/TulipApi.java
+++ tulip-runtime/src/main/java/io/github/wfouche/tulip/api/TulipApi.java
@@ -19,7 +19,7 @@ public class TulipApi {
     /**
      * The version string of the Tulip API.
      */
-    public static final String VERSION = "2.0.1-dev";
+    public static final String VERSION = "2.0.1";
 
     /**
      * A banner displaying the Tulip logo in ASCII art.
