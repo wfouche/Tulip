52f876899b3c8d96faa5824de6a0ff3899a51b2b
Werner Fouché <werner.fouche@gmail.com>
2024-12-07 08:05:53 +0200

    Use w3m instead of lynx for text reports from HTML
    w3m doesn't follow hrefs; its simpler output is preferred

--- tulip-standalone/demo-jbang/run_bench.sh
+++ tulip-standalone/demo-jbang/run_bench.sh
@@ -3,4 +3,5 @@ rm -f benchmark_report.html
 export JBANG_JAVA_OPTIONS="-server -Xmx1024m -XX:+UseZGC -XX:+ZGenerational"
 jbang run App.kt --config=benchmark_config.json
 echo ""
-lynx -dump -width 200 benchmark_report.html
+w3m -dump -cols 200 benchmark_report.html
+#lynx -dump -width 200 benchmark_report.html
