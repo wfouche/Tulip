9177e954710090f7646aadf92ec0ff022cfe1d56
Werner Fouché <werner.fouche@gmail.com>
2024-12-10 08:33:31 +0200

    Fixed path to benchmark_report.html

--- tulip-standalone/oppwa-gradle/run_bench.sh
+++ tulip-standalone/oppwa-gradle/run_bench.sh
@@ -1,4 +1,4 @@
 ./gradlew run --args="--config=benchmark_config.json"
 echo ""
-w3m -dump -cols 200 benchmark_report.html
+w3m -dump -cols 200 app/benchmark_report.html
 #lynx -dump -width 200 app/benchmark_report.html
