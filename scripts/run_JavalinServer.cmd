36bdcb8e209f4869e42990d09b4d6ab27d59f176
Werner Fouché <werner.fouche@gmail.com>
2024-12-13 07:02:03 +0200

    Created run JavalinServer.cmd

---
+++ scripts/run_JavalinServer.cmd
@@ -0,0 +1,10 @@
+REM Shell 1 - run JavalinServer
+REM
+set JBANG_JAVA_OPTIONS=-server -Xms2048m -Xmx2048m -XX:+UseZGC -XX:+ZGenerational -XX:+UseDynamicNumberOfGCThreads
+
+jbang JavalinServer.java
+
+REM Shell 2 - benchmark JavalinServer with Apache Bench
+REM ab -c 4 -t 5 http://localhost:7070/hello
+
+REM Shell 3 - benchmark JavalinServer with Tulip
