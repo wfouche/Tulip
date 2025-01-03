49ec52ed3f6f265dd72524dff77a34b041faed3b
Werner Fouché <werner.fouche@gmail.com>
2025-01-03 17:52:04 +0200

    Starting release 2.0.5

--- tulip-runtime/build.gradle.kts
+++ tulip-runtime/build.gradle.kts
@@ -5,7 +5,7 @@ import org.jreleaser.model.Active
 import org.jetbrains.dokka.gradle.DokkaTask
 
 group = "io.github.wfouche.tulip"
-version = "2.0.4"
+version = "2.0.5"
 
 plugins {
     id("com.github.ben-manes.versions") version "0.51.0"
--- tulip-runtime/src/main/java/io/github/wfouche/tulip/api/TulipApi.java
+++ tulip-runtime/src/main/java/io/github/wfouche/tulip/api/TulipApi.java
@@ -19,7 +19,7 @@ public class TulipApi {
     /**
      * The version string of the Tulip API.
      */
-    public static final String VERSION = "2.0.4";
+    public static final String VERSION = "2.0.5";
 
     /**
      * A banner displaying the Tulip logo in ASCII art.
