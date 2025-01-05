544cb97ff3e571cda877d5eec7f15507a559adca
Werner Fouché <werner.fouche@gmail.com>
2025-01-05 07:10:35 +0200

    Pause after remove local tulip jars

--- publishToMavenLocal.cmd
+++ publishToMavenLocal.cmd
@@ -1,4 +1,5 @@
 dir /s %USERPROFILE%\.m2\tulip-runtime*.jar
 rd/q/s %USERPROFILE%\.m2\repository\io\github\wfouche\tulip\tulip-runtime
+pause
 call .\gradlew :tulip-runtime:publishToMavenLocal
 dir /s %USERPROFILE%\.m2\tulip-runtime*.jar
