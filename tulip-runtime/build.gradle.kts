076b44f865711a2080bfa2db69c393beed09813e
Werner Fouché <werner.fouche@gmail.com>
2025-01-08 10:42:59 +0200

    Dokka version upgrade to 2.0.0

--- tulip-runtime/build.gradle.kts
+++ tulip-runtime/build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
     `maven-publish`
     id("org.jreleaser") version "1.15.0"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21"
-    id("org.jetbrains.dokka") version "1.9.20"
+    id("org.jetbrains.dokka") version "2.0.0"
 }
 
 java {
