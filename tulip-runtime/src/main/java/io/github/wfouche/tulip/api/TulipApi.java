ddadfe5a9c029b7df7271d54ef73bed5719c25e0
Werner Fouché <werner.fouche@gmail.com>
2025-01-12 16:09:03 +0200

    Display config report and benchmark report

--- tulip-runtime/src/main/java/io/github/wfouche/tulip/api/TulipApi.java
+++ tulip-runtime/src/main/java/io/github/wfouche/tulip/api/TulipApi.java
@@ -252,8 +252,9 @@ public class TulipApi {
             @echo off
             echo.
             REM call w3m.exe -dump -cols 205 benchmark_report.html
-            call benchmark_report.html
+            start benchmark_report.html
             jbang run https://gist.github.com/wfouche/70738de122128bbc19ea888799151699 benchmark_config.adoc
+            start benchmark_config.html
             """;
 
     // jbang io.github.wfouche.tulip:tulip-runtime:2.1.2-dev
