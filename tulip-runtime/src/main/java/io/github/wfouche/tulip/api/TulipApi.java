19a6703324f9eaa44bd471a5c5d3ee96cf04eef1
Werner Fouché <werner.fouche@gmail.com>
2025-01-12 22:23:40 +0200

    Changed workflow probabilities

--- tulip-runtime/src/main/java/io/github/wfouche/tulip/api/TulipApi.java
+++ tulip-runtime/src/main/java/io/github/wfouche/tulip/api/TulipApi.java
@@ -167,8 +167,8 @@ public class TulipApi {
                 "workflows": {
                     "random": {
                         "-": {
-                            "1": 0.10,
-                            "2": 0.90
+                            "1": 0.90,
+                            "2": 0.10
                         },
                         "1": {
                             "-": 1.0
@@ -193,6 +193,19 @@ public class TulipApi {
                public static void main(String[] args) {
                   TulipApi.runTulip("benchmark_config.jsonc");
                }
+            }
+            """;
+
+    private static String kotlinApp = """
+            ///usr/bin/env jbang "$0" "$@" ; exit $?
+            //DEPS io.github.wfouche.tulip:tulip-runtime:__TULIP_VERSION__
+            //SOURCES DemoUser.kt
+            //JAVA 21
+            
+            import io.github.wfouche.tulip.api.TulipApi
+            
+            fun main(args: Array<String>) {
+                TulipApi.runTulip("benchmark_config.jsonc")
             }
             """;
 
@@ -231,7 +244,37 @@ public class TulipApi {
             }
             """;
 
-    private static String runBenchSh = """
+    private static String kotlinUser = """
+            import io.github.wfouche.tulip.api.TulipUser
+        
+            class DemoUser(userId: Int, threadId: Int) : TulipUser(userId, threadId) {
+            
+                override fun onStart(): Boolean {
+                    return true
+                }
+            
+                override fun action1(): Boolean {
+                    Thread.sleep(10)
+                    return true
+                }
+            
+                override fun action2(): Boolean {
+                    Thread.sleep(20)
+                    return true
+                }
+            
+                override fun action3(): Boolean {
+                    return true
+                }
+            
+                override fun onStop(): Boolean {
+                    return true
+                }
+            }
+            """;
+
+
+    private static String runBenchShJava = """
             #!/bin/bash
             rm -f benchmark_report.html
             export JBANG_JAVA_OPTIONS="-server -Xmx1024m -XX:+UseZGC -XX:+ZGenerational"
@@ -242,10 +285,33 @@ public class TulipApi {
             #jbang run https://gist.github.com/wfouche/70738de122128bbc19ea888799151699 benchmark_config.adoc
             """;
 
-    private static String runBenchCmd = """
+    private static String runBenchCmdJava = """
             del benchmark_report.html
             set JBANG_JAVA_OPTIONS=-server -Xmx1024m -XX:+UseZGC -XX:+ZGenerational
             call jbang run App.java
+            @echo off
+            echo.
+            REM call w3m.exe -dump -cols 205 benchmark_report.html
+            start benchmark_report.html
+            jbang run https://gist.github.com/wfouche/70738de122128bbc19ea888799151699 benchmark_config.adoc
+            start benchmark_config.html
+            """;
+
+    private static String runBenchShKotlin = """
+            #!/bin/bash
+            rm -f benchmark_report.html
+            export JBANG_JAVA_OPTIONS="-server -Xmx1024m -XX:+UseZGC -XX:+ZGenerational"
+            jbang run App.kt
+            echo ""
+            w3m -dump -cols 205 benchmark_report.html
+            #lynx -dump -width 205 benchmark_report.html
+            #jbang run https://gist.github.com/wfouche/70738de122128bbc19ea888799151699 benchmark_config.adoc
+            """;
+
+    private static String runBenchCmdKotlin = """
+            del benchmark_report.html
+            set JBANG_JAVA_OPTIONS=-server -Xmx1024m -XX:+UseZGC -XX:+ZGenerational
+            call jbang run App.kt
             @echo off
             echo.
             REM call w3m.exe -dump -cols 205 benchmark_report.html
@@ -261,21 +327,55 @@ public class TulipApi {
      */
     public static void main(String[] args) {
         // jbang io.github.wfouche.tulip:tulip-runtime:<version>
-        System.out.println("Tulip: creating a Java benchmark with JBang support");
-        writeToFile("benchmark_config.jsonc", benchmarkConfig.stripLeading(), false);
-        writeToFile("App.java", javaApp.stripLeading().replace("__TULIP_VERSION__", VERSION), false);
-        writeToFile("DemoUser.java", javaUser.stripLeading(), false);
-        writeToFile("run_bench.sh", runBenchSh.stripLeading(), false);
-        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
-            // pass
-        } else {
-            try {
-                String[] cmdArray = {"chmod", "u+x", "run_bench.sh"};
-                Runtime.getRuntime().exec(cmdArray);
-            } catch (IOException e) {
-                // pass
-            }
+        String lang = "Java";
+        if (args.length > 0) {
+            lang = args[0];
         }
-        writeToFile("run_bench.cmd", runBenchCmd.stripLeading(), false);
+        var list = new java.util.ArrayList<String>();
+        list.add("Java");
+        list.add("Kotlin");
+        if (list.contains(lang)) {
+            lang = lang;
+        } else {
+            lang = "Java";
+        }
+
+        System.out.println("Tulip: creating a " + lang + " benchmark with JBang support");
+
+        if (lang.equals("Java")) {
+            writeToFile("benchmark_config.jsonc", benchmarkConfig.stripLeading(), false);
+            writeToFile("App.java", javaApp.stripLeading().replace("__TULIP_VERSION__", VERSION), false);
+            writeToFile("DemoUser.java", javaUser.stripLeading(), false);
+            writeToFile("run_bench.sh", runBenchShJava.stripLeading(), false);
+            if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+                // pass
+            } else {
+                try {
+                    String[] cmdArray = {"chmod", "u+x", "run_bench.sh"};
+                    Runtime.getRuntime().exec(cmdArray);
+                } catch (IOException e) {
+                    // pass
+                }
+            }
+            writeToFile("run_bench.cmd", runBenchCmdJava.stripLeading(), false);
+        }
+
+        if (lang.equals("Kotlin")) {
+            writeToFile("benchmark_config.jsonc", benchmarkConfig.stripLeading(), false);
+            writeToFile("App.kt", kotlinApp.stripLeading().replace("__TULIP_VERSION__", VERSION), false);
+            writeToFile("DemoUser.kt", kotlinUser.stripLeading(), false);
+            writeToFile("run_bench.sh", runBenchShKotlin.stripLeading(), false);
+            if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+                // pass
+            } else {
+                try {
+                    String[] cmdArray = {"chmod", "u+x", "run_bench.sh"};
+                    Runtime.getRuntime().exec(cmdArray);
+                } catch (IOException e) {
+                    // pass
+                }
+            }
+            writeToFile("run_bench.cmd", runBenchCmdKotlin.stripLeading(), false);
+        }
     }
 }
