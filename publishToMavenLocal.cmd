2c65a11938bc1d50fbd5eac9047edcd70bef631b
Werner Fouché <werner.fouche@gmail.com>
2025-01-12 18:49:23 +0200

    Display jbang command to use to create a new project (Windows)

--- publishToMavenLocal.cmd
+++ publishToMavenLocal.cmd
@@ -3,3 +3,5 @@ rd/q/s %USERPROFILE%\.m2\repository\io\github\wfouche\tulip\tulip-runtime
 pause
 call .\gradlew :tulip-runtime:publishToMavenLocal
 dir /s %USERPROFILE%\.m2\tulip-runtime*.jar
+echo.
+echo "jbang io.github.wfouche.tulip:tulip-runtime:<version>"
