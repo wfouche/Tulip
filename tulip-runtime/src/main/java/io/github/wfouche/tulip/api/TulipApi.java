2a588b9e64240378a0e670e0765b0a8b4ae3d1fb
Werner Fouché <werner.fouche@gmail.com>
2025-01-14 12:29:37 +0200

    New REST2 benchmark in Java

--- tulip-runtime/src/main/java/io/github/wfouche/tulip/api/TulipApi.java
+++ tulip-runtime/src/main/java/io/github/wfouche/tulip/api/TulipApi.java
@@ -91,15 +91,17 @@ public class TulipApi {
                     "user_class": "HttpUser",
                     "user_params": {
                         "baseURI": "https://jsonplaceholder.typicode.com",
+                        "baseURI2": "http://localhost:7071",
                         "tracing": false,
-                        "http_port": 7070
+                        "http_port": 7071
                     },
                     "user_actions": {
                         "0": "onStart",
                         "1": "DELAY-10ms",
                         "2": "DELAY-20ms",
-                        "3": "None",
-                        "4": "GET:posts",
+                        "3": "No-op",
+                        "4": "GET:posts(RA)",
+                        "5": "GET:posts(HC)",
                         "99": "onStop"
                     }
                 },
@@ -145,7 +147,7 @@ public class TulipApi {
                         "throughput_rate": 100.0,
                         "workflow": "random"
                     },
-                     "REST": {
+                     "REST1": {
                         "enabled": true,
                         "time": {
                             "pre_warmup_duration": 5,
@@ -157,6 +159,21 @@ public class TulipApi {
                         "actions": [
                             {
                                 "id": 4
+                            }
+                        ]
+                    },
+                     "REST2": {
+                        "enabled": true,
+                        "time": {
+                            "pre_warmup_duration": 5,
+                            "warmup_duration": 10,
+                            "benchmark_duration": 30,
+                            "benchmark_repeat_count": 3
+                        },
+                        "throughput_rate": 0.0,
+                        "actions": [
+                            {
+                                "id": 5
                             }
                         ]
                     },
@@ -232,7 +249,13 @@ public class TulipApi {
 
     private static String javaUser = """
             import io.github.wfouche.tulip.api.*;
+            
             import static io.restassured.RestAssured.*;
+            
+            import java.net.URI;
+            import java.net.http.HttpClient;
+            import java.net.http.HttpRequest;
+            import java.net.http.HttpResponse;
             
             public class HttpUser extends TulipUser {
             
@@ -248,26 +271,29 @@ public class TulipApi {
                     return true;
                 }
             
+                // Action 1: delay 10ms
                 public boolean action1() {
                     try { Thread.sleep(10); } catch (InterruptedException e) { e.printStackTrace(); }
                     return true;
                 }
             
+                // Action 2: delay 20ms
                 public boolean action2() {
                     try { Thread.sleep(20); } catch (InterruptedException e) { e.printStackTrace(); }
                     return true;
                 }
             
+                // Action 3: No-op
                 public boolean action3() {
                     return true;
                 }
             
+                // Action 4: GET /posts/{id} using Rest-Assured
                 public boolean action4() {
                     boolean rc = true;
                     try {
                         given()
                         .when()
-                            //.get("/posts/1")
                             .get("/posts/" + Integer.toString(getUserId()+1))
                         .then()
                             .statusCode(200);
@@ -277,8 +303,40 @@ public class TulipApi {
                     return rc;
                 }
             
+                // Action 5: GET /posts/{id} using java.net.http.HttpClient
+                public boolean action5() {
+                    boolean rc = true;
+                    try {
+                        var response = client.send(httpRequestPosts, HttpResponse.BodyHandlers.ofString());
+                        rc =  (response.statusCode() == 200);
+                    } catch (java.lang.Exception e) {
+                        rc = false;
+                    }
+                    return rc;
+                }
+            
                 public boolean onStop() {
                     return true;
+                }
+            
+                // Action 5 support data and methods
+                private static HttpClient client = HttpClient.newHttpClient();
+            
+                private HttpRequest httpRequestPosts = createHttpRequest("posts");
+            
+                private HttpRequest createHttpRequest(String name) {
+                    try {
+                        var id = getUserId() + 1;
+                        var url = this.getUserParamValue("baseURI");
+                        var request = HttpRequest.newBuilder()
+                                .uri(new URI(url + "/" + name + "/" + String.valueOf(id)))
+                                .GET()
+                                .build();
+                        return request;
+                    } catch (java.lang.Exception e) {
+                        throw new RuntimeException(e);
+                    }
+            
                 }
             
             }
