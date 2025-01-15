e7befbc0eb5af99be9e07f984da8b1eda402d2d1
Werner Fouché <werner.fouche@gmail.com>
2025-01-15 13:11:54 +0200

    Demo file clean up

--- sandbox/demo/run_bench.cmd
+++
@@ -1,9 +0,0 @@
-del benchmark_report.html
-set JBANG_JAVA_OPTIONS=-server -Xmx1024m -XX:+UseZGC -XX:+ZGenerational
-call jbang run App.java
-@echo off
-echo.
-REM call w3m.exe -dump -cols 205 benchmark_report.html
-start benchmark_report.html
-jbang run https://gist.github.com/wfouche/70738de122128bbc19ea888799151699 benchmark_config.adoc
-start benchmark_config.html
--- sandbox/demo/run_bench.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm -f benchmark_report.html
-export JBANG_JAVA_OPTIONS="-server -Xmx1024m -XX:+UseZGC -XX:+ZGenerational"
-jbang run App.java
-echo ""
-w3m -dump -cols 205 benchmark_report.html
-#lynx -dump -width 205 benchmark_report.html
-#jbang run https://gist.github.com/wfouche/70738de122128bbc19ea888799151699 benchmark_config.adoc
