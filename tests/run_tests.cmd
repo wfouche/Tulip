e6dd936bf180a4d980a0b682f96790f41cc0e2ac
Werner Fouché <werner.fouche@gmail.com>
2025-01-18 18:06:46 +0200

    Set Tulip version globally

--- tests/run_tests.cmd
+++ tests/run_tests.cmd
@@ -1,23 +1,25 @@
+set version=2.1.3-dev
+
 md Java
 cd Java
-call jbang io.github.wfouche.tulip:tulip-runtime:2.1.3-dev Java
+call jbang io.github.wfouche.tulip:tulip-runtime:%version% Java
 call run_bench.cmd
 cd ..
 
 md Groovy
 cd Groovy
-call jbang io.github.wfouche.tulip:tulip-runtime:2.1.3-dev Groovy
+call jbang io.github.wfouche.tulip:tulip-runtime:%version% Groovy
 call run_bench.cmd
 cd ..
 
 md Kotlin
 cd Kotlin
-call jbang io.github.wfouche.tulip:tulip-runtime:2.1.3-dev Kotlin
+call jbang io.github.wfouche.tulip:tulip-runtime:%version% Kotlin
 call run_bench.cmd
 cd ..
 
 md Scala
 cd Scala
-call jbang io.github.wfouche.tulip:tulip-runtime:2.1.3-dev Scala
+call jbang io.github.wfouche.tulip:tulip-runtime:%version% Scala
 call run_bench.cmd
 cd ..
