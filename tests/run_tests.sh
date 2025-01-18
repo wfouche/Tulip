83fa31f06b0c02f9a002de5d3932345b97a7bd38
Werner Fouché <werner.fouche@gmail.com>
2025-01-18 18:03:26 +0200

    Set Tulip version globally

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,23 +1,25 @@
+export TULIP_VERSION="2.1.3-dev"
+
 mkdir -p Java
 cd Java
-jbang io.github.wfouche.tulip:tulip-runtime:2.1.3-dev Java
+jbang io.github.wfouche.tulip:tulip-runtime:$TULIP_VERSION Java
 . ./run_bench.sh
 cd ..
 
 mkdir -p Groovy
 cd Groovy
-jbang io.github.wfouche.tulip:tulip-runtime:2.1.3-dev Groovy
+jbang io.github.wfouche.tulip:tulip-runtime:$TULIP_VERSION Groovy
 . ./run_bench.sh
 cd ..
 
 mkdir -p Kotlin
 cd Kotlin
-jbang io.github.wfouche.tulip:tulip-runtime:2.1.3-dev Kotlin
+jbang io.github.wfouche.tulip:tulip-runtime:$TULIP_VERSION Kotlin
 . ./run_bench.sh
 cd ..
 
 mkdir -p Scala
 cd Scala
-jbang io.github.wfouche.tulip:tulip-runtime:2.1.3-dev Scala
+jbang io.github.wfouche.tulip:tulip-runtime:$TULIP_VERSION Scala
 . ./run_bench.sh
 cd ..
