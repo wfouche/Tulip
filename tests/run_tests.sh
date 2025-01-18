41477dc54145916a5deaa36b124341ecd7c60e39
Werner Fouché <werner.fouche@gmail.com>
2025-01-18 20:56:33 +0200

    Created TULIP_OPTS variable

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,27 +1,29 @@
 export TULIP_VERSION="2.1.3-dev"
 
+export TULIP_OPTS="10000.0 http://localhost:7070"
+
 mkdir -p Java
 cd Java
-jbang io.github.wfouche.tulip:tulip-runtime:$TULIP_VERSION Java 1000.0 http://localhost:7070
+jbang io.github.wfouche.tulip:tulip-runtime:$TULIP_VERSION Java $TULIP_OPTS
 . ./run_bench.sh
 cd ..
 
 mkdir -p Groovy
 cd Groovy
-jbang io.github.wfouche.tulip:tulip-runtime:$TULIP_VERSION Groovy 1000.0 http://localhost:7070
+jbang io.github.wfouche.tulip:tulip-runtime:$TULIP_VERSION Groovy $TULIP_OPTS
 
 . ./run_bench.sh
 cd ..
 
 mkdir -p Kotlin
 cd Kotlin
-jbang io.github.wfouche.tulip:tulip-runtime:$TULIP_VERSION Kotlin 1000.0 http://localhost:7070
+jbang io.github.wfouche.tulip:tulip-runtime:$TULIP_VERSION Kotlin $TULIP_OPTS
 . ./run_bench.sh
 cd ..
 
 mkdir -p Scala
 cd Scala
-jbang io.github.wfouche.tulip:tulip-runtime:$TULIP_VERSION Scala 1000.0 http://localhost:7070
+jbang io.github.wfouche.tulip:tulip-runtime:$TULIP_VERSION Scala $TULIP_OPTS
 . ./run_bench.sh
 cd ..
 
