b0e8e73ba42b79f064b637b362e585cd84a817c7
Werner Fouché <werner.fouche@gmail.com>
2025-01-19 17:17:14 +0200

    update maven central release build scripts

--- buildMavenCentralArtifacts.sh
+++ buildMavenCentralArtifacts.sh
@@ -3,7 +3,7 @@ source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk env
 
 # Tulip version
-export version="2.1.2"
+export version="2.1.3"
 
 # Remove staging folder
 rm -f -r tulip-runtime/build/staging-deploy
