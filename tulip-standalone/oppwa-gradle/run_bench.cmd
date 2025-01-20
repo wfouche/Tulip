cc1ca27e6b2f76b4755599322d972793c3d0ec2f
Werner Fouché <werner.fouche@gmail.com>
2025-01-20 12:06:09 +0200

    Removed old test benchmark

--- tulip-standalone/oppwa-gradle/run_bench.cmd
+++
@@ -1,4 +0,0 @@
-call .\gradlew -q run --args="--config benchmark_config.jsonc"
-@echo off
-echo.
-..\..\platform\msys64\bin\w3m.exe -dump -cols 205 app/benchmark_report.html
--- tulip-standalone/oppwa-gradle/run_bench.sh
+++
@@ -1,4 +0,0 @@
-./gradlew run --args="--config=benchmark_config.jsonc"
-echo ""
-w3m -dump -cols 205 app/benchmark_report.html
-#lynx -dump -width 205 app/benchmark_report.html
