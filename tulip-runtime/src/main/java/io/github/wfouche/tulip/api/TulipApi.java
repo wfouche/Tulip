2115be05707fa8b41233d9909e3c469bf2cebcc2
Werner Fouché <werner.fouche@gmail.com>
2025-01-21 20:52:09 +0200

    Change version to 2.1.4

--- tulip-runtime/src/main/java/io/github/wfouche/tulip/api/TulipApi.java
+++ tulip-runtime/src/main/java/io/github/wfouche/tulip/api/TulipApi.java
@@ -21,7 +21,7 @@ public class TulipApi {
     /**
      * The version string of the Tulip API.
      */
-    public static final String VERSION = "2.1.4-dev";
+    public static final String VERSION = "2.1.4";
 
     /**
      * A banner displaying the Tulip logo in ASCII art.
