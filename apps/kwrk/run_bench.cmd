12b78c4f7899f4eccc96cf1a46f79dab2e6d98ce
Werner Fouché <werner.fouche@gmail.com>
2025-01-21 11:18:17 +0200

    Script clean up

--- apps/kwrk/run_bench.cmd
+++ apps/kwrk/run_bench.cmd
@@ -1,7 +1,7 @@
-REM jbang io.github.wfouche.tulip:tulip-runtime:2.1.3 Java
+REM kwrk
 if exist benchmark_report.html del benchmark_report.html
 set JBANG_JAVA_OPTIONS=-server -Xms2g -Xmx2g -XX:+UseZGC -XX:+ZGenerational
-call jbang run kwrk.kt --rate 10000.0 --threads 2 --duration 20 --repeat 4 --url https://jsonplaceholder.typicode.com/posts/1
+jbang run kwrk.kt --rate 10000.0 --threads 2 --duration 30 --repeat 4 --url https://jsonplaceholder.typicode.com/posts/1
 @echo off
 echo.
 REM w3m.exe -dump -cols 205 benchmark_report.html
