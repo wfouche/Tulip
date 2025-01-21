8f21539d099e3edd9b626e5de936552d74f0b893
Werner Fouché <werner.fouche@gmail.com>
2025-01-21 09:35:34 +0200

    Script to run kwirk on Windows

---
+++ apps/kwirk/run_bench.cmd
@@ -0,0 +1,12 @@
+REM jbang io.github.wfouche.tulip:tulip-runtime:2.1.3 Java
+if exist benchmark_report.html del benchmark_report.html
+set JBANG_JAVA_OPTIONS=-server -Xms2g -Xmx2g -XX:+UseZGC -XX:+ZGenerational
+call jbang run kwrik.kt --rate 10000.0 --threads 2 --duration 20 --repeat 4 --url https://jsonplaceholder.typicode.com/posts/1
+@echo off
+echo.
+REM w3m.exe -dump -cols 205 benchmark_report.html
+REM lynx.exe -dump -width 205 benchmark_report.html
+start benchmark_report.html
+REM jbang run https://gist.github.com/wfouche/70738de122128bbc19ea888799151699 benchmark_config.adoc
+REM start benchmark_config.html
+
