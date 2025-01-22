b8d264e53a34598d472b4218714674bb8e6508a2
Werner Fouché <werner.fouche@gmail.com>
2025-01-22 20:28:05 +0200

    Added conditional folder creation logic

--- tests/run_tests.cmd
+++ tests/run_tests.cmd
@@ -1,24 +1,24 @@
 set version=2.1.4
 
-md Java
+if not exist "Java" (md Java)
 cd Java
 call jbang io.github.wfouche.tulip:tulip-runtime:%version% Java
 call run_bench.cmd
 cd ..
 
-md Groovy
+if not exist "Groovy" (md Groovy)
 cd Groovy
 call jbang io.github.wfouche.tulip:tulip-runtime:%version% Groovy
 call run_bench.cmd
 cd ..
 
-md Kotlin
+if not exist "Kotlin" (md Kotlin)
 cd Kotlin
 call jbang io.github.wfouche.tulip:tulip-runtime:%version% Kotlin
 call run_bench.cmd
 cd ..
 
-md Scala
+if not exist "Scala" (md Scala)
 cd Scala
 call jbang io.github.wfouche.tulip:tulip-runtime:%version% Scala
 call run_bench.cmd
