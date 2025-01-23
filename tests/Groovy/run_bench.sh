c9dba8254abbcd05fca42394d8f0b6d9d4b6d44f
Werner Fouché <werner.fouche@gmail.com>
2025-01-23 14:28:21 +0200

    Removed files

--- tests/Groovy/run_bench.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# jbang io.github.wfouche.tulip:tulip-runtime:2.1.4 Groovy
-rm -f benchmark_report.html
-export JBANG_JAVA_OPTIONS="-server -Xms2g -Xmx2g -XX:+UseZGC -XX:+ZGenerational"
-jbang run App.groovy
-echo ""
-#w3m -dump -cols 205 benchmark_report.html
-lynx -dump -width 205 benchmark_report.html
-#jbang run asciidoc@wfouche benchmark_config.adoc
--- tests/Scala/run_bench.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# jbang io.github.wfouche.tulip:tulip-runtime:2.1.4 Scala
-rm -f benchmark_report.html
-scala-cli App.scala HttpUser.scala
-echo ""
-#w3m -dump -cols 205 benchmark_report.html
-lynx -dump -width 205 benchmark_report.html
-#jbang run asciidoc@wfouche benchmark_config.adoc
