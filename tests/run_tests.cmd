47da0dfe496cc8db3c55aad48e94854b94ea5dc8
Werner Fouché <werner.fouche@gmail.com>
2025-01-24 15:52:22 +0200

    Added options for Windows

--- tests/run_tests.cmd
+++ tests/run_tests.cmd
@@ -1,25 +1,26 @@
-set version=2.1.5-dev
+set TULIP_VERSION=2.1.5-dev
+set TULIP_OPTS=10000.0 http://localhost:7070
 
 if not exist "Java" (md Java)
 cd Java
-call jbang io.github.wfouche.tulip:tulip-runtime:%version% Java
+call jbang io.github.wfouche.tulip:tulip-runtime:%TULIP_VERSION% Java %TULIP_OPTS%
 call run_bench.cmd
 cd ..
 
 if not exist "Groovy" (md Groovy)
 cd Groovy
-call jbang io.github.wfouche.tulip:tulip-runtime:%version% Groovy
+call jbang io.github.wfouche.tulip:tulip-runtime:%TULIP_VERSION% Groovy %TULIP_OPTS%
 call run_bench.cmd
 cd ..
 
 if not exist "Kotlin" (md Kotlin)
 cd Kotlin
-call jbang io.github.wfouche.tulip:tulip-runtime:%version% Kotlin
+call jbang io.github.wfouche.tulip:tulip-runtime:%TULIP_VERSION% Kotlin %TULIP_OPTS%
 call run_bench.cmd
 cd ..
 
 if not exist "Scala" (md Scala)
 cd Scala
-call jbang io.github.wfouche.tulip:tulip-runtime:%version% Scala
+call jbang io.github.wfouche.tulip:tulip-runtime:%TULIP_VERSION% Scala %TULIP_OPTS%
 call run_bench.cmd
 cd ..
