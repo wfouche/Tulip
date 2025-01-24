07ebce6c87da20ef957ddf8f5d56fcb199cfbd26
Werner Fouché <werner.fouche@gmail.com>
2025-01-24 08:49:30 +0200

    Started new development sprint

--- tulip-runtime/build.gradle.kts
+++ tulip-runtime/build.gradle.kts
@@ -5,7 +5,7 @@ import org.jreleaser.model.Active
 import org.jetbrains.dokka.gradle.DokkaTask
 
 group = "io.github.wfouche.tulip"
-version = "2.1.4"
+version = "2.1.5-dev"
 
 plugins {
     id("com.github.ben-manes.versions") version "0.51.0"
