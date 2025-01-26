82e665621dc3494f8b835c8b667e615540f1bd23
Werner Fouché <werner.fouche@gmail.com>
2025-01-26 10:39:29 +0200

    Delete language specific folders before generating code

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,6 +1,7 @@
 export TULIP_VERSION="2.1.5-dev"
 export TULIP_OPTS="10000.0 http localhost:7070"
 
+rm -f -r Java
 mkdir -p Java
 cd Java || exit
 echo ""
@@ -8,6 +9,7 @@ jbang io.github.wfouche.tulip:tulip-runtime:$TULIP_VERSION Java $TULIP_OPTS
 . ./run_bench.sh
 cd ..
 
+rm -f -r Groovy
 mkdir -p Groovy
 cd Groovy || exit
 echo ""
@@ -16,6 +18,7 @@ jbang io.github.wfouche.tulip:tulip-runtime:$TULIP_VERSION Groovy $TULIP_OPTS
 . ./run_bench.sh
 cd ..
 
+rm -f -r Kotlin
 mkdir -p Kotlin
 cd Kotlin || exit
 echo ""
@@ -23,6 +26,7 @@ jbang io.github.wfouche.tulip:tulip-runtime:$TULIP_VERSION Kotlin $TULIP_OPTS
 . ./run_bench.sh
 cd ..
 
+rm -f -r Scala
 mkdir -p Scala
 cd Scala || exit
 echo ""
