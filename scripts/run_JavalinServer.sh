1ad2b2635bf9a33919bed01d3edd422c3d69b206
Werner Fouché <werner.fouche@gmail.com>
2025-01-31 11:56:57 +0200

    Added run command

--- scripts/run_JavalinServer.sh
+++ scripts/run_JavalinServer.sh
@@ -3,7 +3,7 @@
 # export JBANG_JAVA_OPTIONS="-server -Xms1024m -Xmx1024m -XX:+UseZGC -XX:+ZGenerational"
 export JBANG_JAVA_OPTIONS="-server -Xms1024m -Xmx1024m -XX:+UseParallelGC"
 
-jbang JavalinServer.java
+jbang run JavalinServer.java
 
 # Shell 2 - benchmark JavalinServer with Apache Bench
 # ab -c 4 -t 5 http://localhost:7070/hello
