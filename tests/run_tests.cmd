c694f25b52e730d9fd45c8d68902f9f4ab3bacc4
Werner Fouché <werner.fouche@gmail.com>
2025-02-02 21:11:38 +0200

    New runtime options for demo benchmarks

--- tests/run_tests.cmd
+++ tests/run_tests.cmd
@@ -1,26 +1,28 @@
 set TULIP_VERSION=2.1.5
-set TULIP_OPTS=10000.0 http localhost:7070
+set TULIP_PARAMS=10000.0 http localhost:7070
+
+set TULIP_JAVA_OPTIONS=-server -Xms2g -Xmx2g -XX:+UseZGC -XX:+ZGenerational
 
 if not exist "Java" (md Java)
 cd Java
-call jbang io.github.wfouche.tulip:tulip-runtime:%TULIP_VERSION% init Java %TULIP_OPTS%
+call jbang io.github.wfouche.tulip:tulip-runtime:%TULIP_VERSION% init Java %TULIP_PARAMS%
 call run_bench.cmd
 cd ..
 
 if not exist "Groovy" (md Groovy)
 cd Groovy
-call jbang io.github.wfouche.tulip:tulip-runtime:%TULIP_VERSION% init Groovy %TULIP_OPTS%
+call jbang io.github.wfouche.tulip:tulip-runtime:%TULIP_VERSION% init Groovy %TULIP_PARAMS%
 call run_bench.cmd
 cd ..
 
 if not exist "Kotlin" (md Kotlin)
 cd Kotlin
-call jbang io.github.wfouche.tulip:tulip-runtime:%TULIP_VERSION% init Kotlin %TULIP_OPTS%
+call jbang io.github.wfouche.tulip:tulip-runtime:%TULIP_VERSION% init Kotlin %TULIP_PARAMS%
 call run_bench.cmd
 cd ..
 
 if not exist "Scala" (md Scala)
 cd Scala
-call jbang io.github.wfouche.tulip:tulip-runtime:%TULIP_VERSION% init Scala %TULIP_OPTS%
+call jbang io.github.wfouche.tulip:tulip-runtime:%TULIP_VERSION% init Scala %TULIP_PARAMS%
 call run_bench.cmd
 cd ..
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,11 +1,27 @@
-export TULIP_VERSION="2.1.5"
-export TULIP_OPTS="10000.0 http localhost:7070"
+#!/bin/bash
+
+# Create a 2 GB regions of Large Pages
+#
+# $ ls /sys/kernel/mm/hugepages/
+#   hugepages-1048576kB  hugepages-2048kB
+#
+# # (As root) Reserve 2 GB of RAM
+# $ echo 1024 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
+#
+# "-XX:+UseLargePages",
+
+export TULIP_VERSION="2.1.6-dev"
+export TULIP_PARAMS="10000.0 http localhost:7070"
+
+unset TULIP_JAVA_OPTIONS
+export TULIP_JAVA_OPTIONS="-server -Xms1g -Xmx1g -XX:+UseZGC -XX:+ZGenerational -XX:+UseLargePages"
+export TULIP_JAVA_OPTIONS="-server -Xms1g -Xmx1g -XX:+UseZGC -XX:+ZGenerational"
 
 rm -f -r Java
 mkdir -p Java
 cd Java || exit
 echo ""
-jbang io.github.wfouche.tulip:tulip-runtime:$TULIP_VERSION init Java $TULIP_OPTS
+jbang io.github.wfouche.tulip:tulip-runtime:$TULIP_VERSION init Java $TULIP_PARAMS
 . ./run_bench.sh
 cd ..
 
@@ -13,7 +29,7 @@ rm -f -r Groovy
 mkdir -p Groovy
 cd Groovy || exit
 echo ""
-jbang io.github.wfouche.tulip:tulip-runtime:$TULIP_VERSION init Groovy $TULIP_OPTS
+jbang io.github.wfouche.tulip:tulip-runtime:$TULIP_VERSION init Groovy $TULIP_PARAMS
 
 . ./run_bench.sh
 cd ..
@@ -22,7 +38,7 @@ rm -f -r Kotlin
 mkdir -p Kotlin
 cd Kotlin || exit
 echo ""
-jbang io.github.wfouche.tulip:tulip-runtime:$TULIP_VERSION init Kotlin $TULIP_OPTS
+jbang io.github.wfouche.tulip:tulip-runtime:$TULIP_VERSION init Kotlin $TULIP_PARAMS
 . ./run_bench.sh
 cd ..
 
@@ -30,7 +46,7 @@ rm -f -r Scala
 mkdir -p Scala
 cd Scala || exit
 echo ""
-jbang io.github.wfouche.tulip:tulip-runtime:$TULIP_VERSION init Scala $TULIP_OPTS
+jbang io.github.wfouche.tulip:tulip-runtime:$TULIP_VERSION init Scala $TULIP_PARAMS
 . ./run_bench.sh
 cd ..
 
