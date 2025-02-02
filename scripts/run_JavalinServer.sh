bd198b0355031f8d381605d52edde1f4c23746f0
Werner Fouché <werner.fouche@gmail.com>
2025-02-02 21:11:25 +0200

    New runtime options for test service

--- scripts/run_JavalinServer.sh
+++ scripts/run_JavalinServer.sh
@@ -1,7 +1,19 @@
 # Shell 1 - run JavalinServer
 #
-# export JBANG_JAVA_OPTIONS="-server -Xms1024m -Xmx1024m -XX:+UseZGC -XX:+ZGenerational"
-export JBANG_JAVA_OPTIONS="-server -Xms1024m -Xmx1024m -XX:+UseParallelGC"
+# Create a 2 GB regions of Large Pages
+#
+# $ ls /sys/kernel/mm/hugepages/
+#   hugepages-1048576kB  hugepages-2048kB
+#
+# # Reserve 2 GB of RAM
+# $ echo 1024 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
+#
+# "-XX:+UseLargePages",
+
+export JBANG_JAVA_OPTIONS="-server -Xms512m -Xmx512m -XX:+UseZGC -XX:+ZGenerational"
+export JBANG_JAVA_OPTIONS="-server -Xms512m -Xmx512m -XX:+UseZGC -XX:+ZGenerational -XX:+UseLargePages"
+export JBANG_JAVA_OPTIONS="-server -Xms512m -Xmx512m             -XX:+UseParallelGC -XX:+UseLargePages"
+export JBANG_JAVA_OPTIONS="-server -Xms512m -Xmx512m             -XX:+UseParallelGC"
 
 jbang run JavalinServer.java
 
