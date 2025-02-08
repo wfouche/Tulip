dc204ec8e9be3aeac53ea06eb14df7fb2669e11f
Werner Fouché <werner.fouche@gmail.com>
2025-02-08 09:29:57 +0200

    Generate html from adoc

--- reports/report2.sh
+++ reports/report2.sh
@@ -1,1 +1,2 @@
 java -Dpython.path=./deps/HdrHistogram-2.2.2.jar:./deps/gson-2.11.0.jar -jar ./deps/jython-standalone-2.7.4.jar  report2.py report2_config.jsonc
+jbang run asciidoc@wfouche report2_config.adoc
