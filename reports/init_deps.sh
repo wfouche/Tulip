9104b26431e14011c639f7042604c9fd7b885865
Werner Fouché <werner.fouche@gmail.com>
2025-02-08 09:09:59 +0200

    Use jpm@codejive to install jar deps

---
+++ reports/init_deps.sh
@@ -0,0 +1,1 @@
+jbang run jpm@codejive install
--- reports/report2.cmd
+++ reports/report2.cmd
@@ -1,1 +1,1 @@
-java -Dpython.path=.\jars\HdrHistogram-2.2.2.jar;.\jars\gson-2.11.0.jar -jar .\jars\jython-standalone-2.7.4.jar  report2.py report2_config.jsonc
+java -Dpython.path=.\deps\HdrHistogram-2.2.2.jar;.\deps\gson-2.11.0.jar -jar .\deps\jython-standalone-2.7.4.jar  report2.py report2_config.jsonc
