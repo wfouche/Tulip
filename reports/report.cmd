3463e0135fd1abfc1767559011e113263fa3c7bb
Werner Fouché <werner.fouche@gmail.com>
2025-02-09 18:42:23 +0200

    Windows reports now use jython-jpm

--- reports/report.cmd
+++ reports/report.cmd
@@ -1,1 +1,1 @@
-java -Dpython.path=.\deps\HdrHistogram-2.2.2.jar -jar .\deps\jython-standalone-2.7.4.jar  report.py ..\tulip-main\benchmark_output.json
+jbang run jython-jpm@wfouche report.py ..\tulip-main\benchmark_output.json
