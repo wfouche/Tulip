7c4bc6b4c2a570e2a5832da1af5d4aa7a3d1f746
Werner Fouché <werner.fouche@gmail.com>
2025-02-10 11:48:03 +0200

    Update Linux Jython scripts to use Jython.java

--- reports/report.sh
+++ reports/report.sh
@@ -1,1 +1,1 @@
-jbang run jython-jpm@wfouche report.py ../tulip-main/benchmark_output.json
+jbang run Jython.java report.py ../tulip-main/benchmark_output.json
--- reports/report2.sh
+++ reports/report2.sh
@@ -1,2 +1,2 @@
-jbang run jython-jpm@wfouche report2.py report2_config.jsonc
+jbang run Jython.java report2.py report2_config.jsonc
 jbang run asciidoc@wfouche report2_config.adoc
