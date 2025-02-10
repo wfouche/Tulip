58aa8562d1bf6d6d38ec42b83ab47faa7c7f9d2a
Werner Fouché <werner.fouche@gmail.com>
2025-02-10 11:47:11 +0200

    Jython script runner written in Java (JBang)

---
+++ reports/Jython.java
@@ -0,0 +1,15 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+
+//DEPS org.python:jython-standalone:2.7.4
+//DEPS com.google.code.gson:gson:2.11.0
+//DEPS org.hdrhistogram:HdrHistogram:2.2.2
+
+import org.python.util.jython;
+
+public class Jython {
+
+    public static void main(String... args) {
+        jython.main(args);
+    }
+
+}
