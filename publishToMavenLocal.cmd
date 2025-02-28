e437a0e35b4413097c3702cf3b606ddb5015aae7
Werner Fouché <werner.fouche@gmail.com>
2025-02-28 20:28:02 +0200

    Removed echo statements

--- publishToMavenLocal.cmd
+++ publishToMavenLocal.cmd
@@ -3,5 +3,5 @@ rd/q/s %USERPROFILE%\.m2\repository\io\github\wfouche\tulip\tulip-runtime
 pause
 call .\gradlew :tulip-runtime:publishToMavenLocal
 dir /s %USERPROFILE%\.m2\tulip-runtime*.jar
-echo.
-echo "jbang io.github.wfouche.tulip:tulip-runtime:<version>"
+REM echo.
+REM echo "jbang io.github.wfouche.tulip:tulip-runtime:<version>"
