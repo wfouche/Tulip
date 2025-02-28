c04a4e4a26b6996fe63376f2b73b995e184e00d4
Werner Fouché <werner.fouche@gmail.com>
2025-02-28 08:14:51 +0200

    Upgrade dependencies

--- buildSrc/src/main/kotlin/buildlogic.kotlin-common-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/buildlogic.kotlin-common-conventions.gradle.kts
@@ -19,7 +19,7 @@ dependencies {
     }
 
     // Use JUnit Jupiter for testing.
-    testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.12.0")
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
