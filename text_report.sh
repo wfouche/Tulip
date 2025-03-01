011d05435c8d5223ad240a5d821418894dc3e86f
Werner Fouché <werner.fouche@gmail.com>
2025-03-01 08:17:49 +0200

    Use lynx to display text report

--- text_report.sh
+++ text_report.sh
@@ -1,6 +1,6 @@
 echo ""
 pushd ./tulip-main
 echo ""
-w3m -dump -cols 205 benchmark_report.html
-#lynx -dump -width 205 benchmark_report.html
+#w3m -dump -cols 205 benchmark_report.html
+lynx -dump -width 205 benchmark_report.html
 popd
