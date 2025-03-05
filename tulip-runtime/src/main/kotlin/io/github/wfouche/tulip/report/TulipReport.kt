f2860c62f04b6258b163fc66afd566d3f48d6e0d
Werner Fouché <werner.fouche@gmail.com>
2025-03-05 19:11:52 +0200

    Renamed Actions to Summary

--- tulip-runtime/src/main/kotlin/io/github/wfouche/tulip/report/TulipReport.kt
+++ tulip-runtime/src/main/kotlin/io/github/wfouche/tulip/report/TulipReport.kt
@@ -257,7 +257,7 @@ def createReport(filename):
             cpu_t = str_from_cpu_time_ns(sm.cpu_time_ns)
 
         statsFilename = '%s_%d.html'%(report_fn.split('.')[0],benchmark_id)
-        text = "<a href='%s'>%s</a>"%(statsFilename,"[Actions]")
+        text = "<a href='%s'>%s</a>"%(statsFilename,"[Summary]")
         printStream = PrintStream(statsFilename)
         printStream.print("<html>")
         printStream.println()
