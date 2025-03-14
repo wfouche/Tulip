82d695db9ca0080f97aa96f5bd8780d9e128d97c
Werner Fouché <werner.fouche@gmail.com>
2025-03-14 13:44:11 +0200

    Moved payment benchmark to Tulip-benchmarks

--- tulip-standalone/oppwa-gradle/gen_report.sh
+++
@@ -1,3 +0,0 @@
-./gradlew run --args="--result=benchmark_output.json"
-
-w3m -dump -cols 200 app/benchmark_report.html
--- tulip-standalone/oppwa-gradle/run_bench.sh
+++
@@ -1,4 +0,0 @@
-./gradlew run --args="--config=oppwa_config.jsonc"
-echo ""
-w3m -dump -cols 205 app/oppwa_report.html
-#lynx -dump -width 205 app/oppwa_report.html
