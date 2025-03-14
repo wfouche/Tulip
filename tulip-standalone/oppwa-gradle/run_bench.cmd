3594695fb1ece1a1bca1f52179c7f5bc4607868d
Werner Fouché <werner.fouche@gmail.com>
2025-03-14 13:25:12 +0200

    Changed config from jsonc to json

--- tulip-standalone/oppwa-gradle/run_bench.cmd
+++ tulip-standalone/oppwa-gradle/run_bench.cmd
@@ -1,4 +1,4 @@
-call .\gradlew -q run --args="--config oppwa_config.jsonc"
+call .\gradlew -q run --args="--config oppwa_config.json"
 @echo off
 echo.
 ..\..\platform\msys64\bin\w3m.exe -dump -cols 205 app/oppwa_report.html
