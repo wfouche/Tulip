68a09bdfe0ffc1a95fced8d9fdde411ffa561ffe
Werner Fouché <werner.fouche@gmail.com>
2025-03-19 17:31:32 +0200

    Updated percentile chart demo

--- reports/echarts/tulip/index_p.js
+++ reports/echarts/tulip/index_p.js
@@ -1,11 +1,11 @@
 var myChart = echarts.init(document.getElementById('main_p'));
 
 var data =  [
-      ['2018-04-10T20:40:33Z', 1, 5],
-      ['2018-04-10T20:40:53Z', 2, 3],
-      ['2018-04-10T20:41:03Z', 4, 2],
-      ['2018-04-10T20:44:03Z', 5, 1],
-      ['2018-04-10T20:45:03Z', 6, 0]
+      ['2018-04-10T20:40:33Z', 1, 5, 10, 20, 25, 30 ],
+      ['2018-04-10T20:40:53Z', 2, 6, 15, 25, 30, 35],
+      ['2018-04-10T20:41:03Z', 2, 5, 20, 30, 35, 40],
+      ['2018-04-10T20:44:03Z', 3, 7, 12, 22, 30, 32],
+      ['2018-04-10T20:45:03Z', 2, 10, 13, 23, 30, 33]
 ];
 
 var option = {
@@ -15,13 +15,13 @@ var option = {
   },
   dataset: {
     source:data,
-    dimensions: ['timestamp', 'sensor1', 'sensor2'],
+    dimensions: ['timestamp', 'sensor1', 'sensor2', 'sensor3', 'sensor4', 'sensor5', 'sensor6'],
   },
   xAxis: { type: 'time' },
   yAxis: { },
   series: [
   {
-     name: '90p',
+     name: 'Min',
      type: 'line',
      smooth: true,
      encode: {
@@ -29,12 +29,46 @@ var option = {
        y: 'sensor1' // refer sensor 1 value 
      }
   },{
+     name: 'Avg',
+     type: 'line',
+     smooth: true,
+     encode: {
+       x: 'timestamp',
+       y: 'sensor2' // refer sensor 1 value
+     }
+  },{
+     name: 'p90',
+     type: 'line',
+     smooth: true,
+     encode: {
+       x: 'timestamp',
+       y: 'sensor3' // refer sensor 1 value
+     }
+
+  },{
+     name: 'p95',
+     type: 'line',
+     smooth: true,
+     encode: {
+       x: 'timestamp',
+       y: 'sensor4' // refer sensor 1 value
+     }
+
+  },{
+     name: 'p99',
+     type: 'line',
+     smooth: true,
+     encode: {
+       x: 'timestamp',
+       y: 'sensor5'
+     }
+  },{
      name: 'Max',
      type: 'line',
      smooth: true,
      encode: {
        x: 'timestamp',
-       y: 'sensor2'
+       y: 'sensor6'
   }
 }]
 };
