54c8a9b45ac06e28c939dd49699097e1b0c5d86d
Werner Fouché <werner.fouche@gmail.com>
2025-04-04 07:08:15 +0200

    Implemented http utility methods

--- tulip-runtime/src/main/java/io/github/wfouche/tulip/user/HttpUser.java
+++ tulip-runtime/src/main/java/io/github/wfouche/tulip/user/HttpUser.java
@@ -1,5 +1,7 @@
 package io.github.wfouche.tulip.user;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
 import org.springframework.web.client.RestClientException;
 
 /** The HttpUser class. */
@@ -22,21 +24,101 @@ public class HttpUser extends HttpUser_RestClient {
    * @param uriVariables - sequence of variables to replace in uri
    * @return boolean
    */
-  public boolean http_GET(String uri, Object... uriVariables) {
-    boolean rc;
+  public String http_GET(String uri, Object... uriVariables) {
     try {
       String rsp = restClient().get().uri(uri, uriVariables).retrieve().body(String.class);
-      rc = (rsp != null && !rsp.isEmpty());
+      if (rsp != null && !rsp.isEmpty()) {
+        return rsp;
+      } else {
+        return "";
+      }
     } catch (RestClientException e) {
-      rc = false;
+      e.printStackTrace();
+      return "";
     }
-    return rc;
+  }
+
+  public String http_POST(String reqBodyText, String uri, Object... uriVariables) {
+    try {
+      String rsp =
+          restClient()
+              .post()
+              .uri(uri, uriVariables)
+              .contentType(APPLICATION_JSON)
+              .body(reqBodyText)
+              .retrieve()
+              .body(String.class);
+      if (rsp != null && !rsp.isEmpty()) {
+        return rsp;
+      } else {
+        return "";
+      }
+    } catch (RestClientException e) {
+      e.printStackTrace();
+      return "";
+    }
+  }
+
+  public String http_PUT(String reqBodyText, String uri, Object... uriVariables) {
+    try {
+      String rsp =
+          restClient()
+              .put()
+              .uri(uri, uriVariables)
+              .contentType(APPLICATION_JSON)
+              .body(reqBodyText)
+              .retrieve()
+              .body(String.class);
+      if (rsp != null && !rsp.isEmpty()) {
+        return rsp;
+      } else {
+        return "";
+      }
+    } catch (RestClientException e) {
+      e.printStackTrace();
+      return "";
+    }
+  }
+
+  public String http_PATCH(String reqBodyText, String uri, Object... uriVariables) {
+    try {
+      String rsp =
+          restClient()
+              .patch()
+              .uri(uri, uriVariables)
+              .contentType(APPLICATION_JSON)
+              .body(reqBodyText)
+              .retrieve()
+              .body(String.class);
+      if (rsp != null && !rsp.isEmpty()) {
+        return rsp;
+      } else {
+        return "";
+      }
+    } catch (RestClientException e) {
+      e.printStackTrace();
+      return "";
+    }
+  }
+
+  public String http_DELETE(String uri, Object... uriVariables) {
+    try {
+      String rsp = restClient().delete().uri(uri, uriVariables).retrieve().body(String.class);
+      if (rsp != null && !rsp.isEmpty()) {
+        return rsp;
+      } else {
+        return "";
+      }
+    } catch (RestClientException e) {
+      e.printStackTrace();
+      return "";
+    }
   }
 
   //    // Action 1: GET /posts/{id}
   //    public boolean action1() {
   //        int id = ThreadLocalRandom.current().nextInt(100)+1;
-  //        return http_GET("/posts/{id}", id);
+  //        return !http_GET("/posts/{id}", id).isEmpty();
   //    }
 
 }
