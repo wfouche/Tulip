d8d042ffe78ae9059dfab9d480d4dfd6d4b4278e
Werner Fouché <werner.fouche@gmail.com>
2025-04-09 18:36:12 +0200

    Embedding script.py file into _script.java file

--- reports/jython/jython-cli.py
+++ reports/jython/jython-cli.py
@@ -1,30 +1,70 @@
 from __future__ import print_function
 import os
 import sys
+import base64
 
 text = """
-
 import org.python.util.jython;
+import org.python.util.PythonInterpreter;
+import java.util.Base64;
 
 public class __CLASSNAME__ {
 
+    public static String mainScriptTextBase64 = "__MAIN_SCRIPT__";
+    
     public static void main(String... args) {
-        jython.main(args);
+        String mainScriptFilename = "__MAIN_SCRIPT_FILENAME__";
+        String mainScript = "";
+        String jythonArgsScript = ""; 
+        for (String arg: args) {
+            //System.out.println("Java: " + arg);
+            if (jythonArgsScript.length() == 0) {
+                if (!arg.equals(mainScriptFilename)) {
+                    jythonArgsScript += "'" + mainScriptFilename + "', ";
+                }
+            } else {
+                jythonArgsScript += ", ";
+            }
+            jythonArgsScript += "'" + arg + "'";
+        }
+        jythonArgsScript = "import sys; sys.argv = [" + jythonArgsScript + "]";
+        {
+            byte[] decodedBytes = Base64.getDecoder().decode(mainScriptTextBase64);
+            String text = new String(decodedBytes);
+            //System.out.println("===");
+            //System.out.println(text);
+            //System.out.println("===");
+            mainScript = text;
+        }
+        //System.out.println("args --> " + jythonArgsScript);
+        {
+            // run script
+            PythonInterpreter pyInterp = new PythonInterpreter();
+            // initialize args
+            pyInterp.exec(jythonArgsScript);
+            // run script
+            pyInterp.exec(mainScript);
+        }
+        //jython.main(args);
     }
 }
 
 """
+
+javaLines = []
 
 def main():
     scriptFilename = sys.argv[1]
     javaClassname = "_" + os.path.basename(scriptFilename)[:-3]
     javaFilename = javaClassname + ".java"
     deps = []
-    version = "2.7.4"
+    jythonVersion = "2.7.4"
+    javaVersion = "21"
     with open(scriptFilename) as f:
         lines = f.readlines()
         tag1 = "##DEPS"
         tag2 = "##JYTHON"
+        tag3 = "##JAVA"
         for line in lines:
             if len(line) > len(tag1):
                 if line[:len(tag1)] == tag1:
@@ -33,16 +73,28 @@ def main():
                     deps.append(dep)
             if len(line) > len(tag2):
                 if line[:len(tag2)] == tag2:
-                    version = line.split()[1]
-    dep = "org.python:jython-standalone:" + version
+                    jythonVersion = line.split()[1]
+            if len(line) > len(tag3):
+                if line[:len(tag3)] == tag3:
+                    javaVersion = line.split()[1]
+            javaLines.append("#" + line)
+
+    dep = "org.python:jython-standalone:" + jythonVersion
     deps.append(dep)
+
+    data = open(scriptFilename,'rb').read()
+    scriptFileTextB64 = base64.b64encode(data).decode('utf-8')
 
     jf = open(javaFilename,"w+")
     jf.write('///usr/bin/env jbang "$0" "$@" ; exit $?' + "\n\n")
     for dep in deps:
         jf.write("//DEPS " + dep + "\n")
         #print(dep)
-    jf.write(text.replace("__CLASSNAME__",javaClassname))
+    jf.write("//JAVA " + javaVersion + "\n")
+    jtext = text.replace("__CLASSNAME__",javaClassname)
+    jtext = jtext.replace("__MAIN_SCRIPT__", scriptFileTextB64)
+    jtext = jtext.replace("__MAIN_SCRIPT_FILENAME__", scriptFilename)
+    jf.write(jtext)
     jf.close()
     #print(sys.argv[1:])
     params = ""
--- reports/jython/test.py
+++ reports/jython/test.py
@@ -1,12 +1,23 @@
 from __future__ import print_function
+import sys
+
+# https://github.com/jbangdev/jbang/issues/1911
+# https://github.com/jython/jython/issues/371
 
 ##DEPS io.leego:banana:2.1.0
-##JYTHON 2.7.3
+##JYTHON 2.7.4
+##JAVA 21
 
 import io.leego.banana.BananaUtils as BananaUtils
 import io.leego.banana.Font as Font
 
-text0 = "Jython 2.7"
-text1 = BananaUtils.bananaify(text0, Font.STANDARD)
 
-print(text1)
+def main():
+    print(sys.argv)
+
+    text0 = "Jython 2.7"
+    text1 = BananaUtils.bananaify(text0, Font.STANDARD)
+
+    print(text1)
+
+main()
