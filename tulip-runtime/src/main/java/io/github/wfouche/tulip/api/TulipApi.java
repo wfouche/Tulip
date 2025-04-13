ef21da5b77babafae14c4b27051af188b258209d
Werner Fouché <werner.fouche@gmail.com>
2025-04-13 07:42:26 +0200

    Tulip 2.1.8-dev

--- tulip-runtime/src/main/java/io/github/wfouche/tulip/api/TulipApi.java
+++ tulip-runtime/src/main/java/io/github/wfouche/tulip/api/TulipApi.java
@@ -15,7 +15,7 @@ public class TulipApi {
   TulipApi() {}
 
   /** The version string of the Tulip API. */
-  public static final String VERSION = "2.1.7";
+  public static final String VERSION = "2.1.8-dev";
 
   /**
    * A banner displaying the Tulip logo in ASCII art.
