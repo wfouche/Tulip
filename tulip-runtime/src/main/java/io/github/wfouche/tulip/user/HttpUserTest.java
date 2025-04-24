b68f02c43ecbcad45784e9928f2fee7562b56a6f
Werner Fouché <werner.fouche@gmail.com>
2025-04-24 12:41:26 +0200

    HttpUserTest class for GraalVM experiments

---
+++ tulip-runtime/src/main/java/io/github/wfouche/tulip/user/HttpUserTest.java
@@ -0,0 +1,21 @@
+package io.github.wfouche.tulip.user;
+
+/** HttpUserTest - experimental class */
+public class HttpUserTest extends HttpUser {
+
+  /** HttpUserTest() constructor */
+  public HttpUserTest() {
+    super(0, 0);
+  }
+
+  /**
+   * init - set base class parameter values
+   *
+   * @param userId
+   * @param threadId
+   */
+  public void init(int userId, int threadId) {
+    setUserId(userId);
+    setThreadId(threadId);
+  }
+}
