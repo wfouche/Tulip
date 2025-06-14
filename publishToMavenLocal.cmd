82d42c96c67b4223f1b4fcdd39a3efbc68bd05f6
Werner Fouché <werner.fouche@gmail.com>
2025-06-14 16:16:26 +0200

    Fixed publishToMavenLocal.cmd script

--- publishToMavenLocal.cmd
+++ publishToMavenLocal.cmd
@@ -1,7 +1,15 @@
 dir /s %USERPROFILE%\.m2\tulip-runtime*.jar
+
 rd/q/s %USERPROFILE%\.m2\repository\io\github\wfouche\tulip\tulip-runtime
+
+pushd .\reports
+call .\update_runtime.cmd
+popd
+
 call .\gradlew spotlessApply
+
 pause
+
 call .\gradlew :tulip-runtime:publishToMavenLocal
 dir /s %USERPROFILE%\.m2\tulip-runtime*.jar
 REM echo.
