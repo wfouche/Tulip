e41d23e5c4a34ba6829c52dfd5e42da2be2c5eee
Werner Fouché <werner.fouche@gmail.com>
2025-06-22 17:37:41 +0200

    Update asciidoctorj-diagram to 3.0.1

--- tulip-runtime/build.gradle.kts
+++ tulip-runtime/build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
 
     // Asciidoctor
     implementation("org.asciidoctor:asciidoctorj:3.0.0")
-    implementation("org.asciidoctor:asciidoctorj-diagram:2.3.2")
+    implementation("org.asciidoctor:asciidoctorj-diagram:3.0.1")
 
     // Google
     implementation("com.google.guava:guava:33.4.8-jre")
