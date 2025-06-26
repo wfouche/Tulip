d81a12a00f3068529a8390309a824a885cfe06f7
Werner Fouché <werner.fouche@gmail.com>
2025-06-26 11:55:11 +0200

    Revert back to asciidoctorj-diagram 2.3.2

--- tulip-runtime/build.gradle.kts
+++ tulip-runtime/build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
 
     // Asciidoctor
     implementation("org.asciidoctor:asciidoctorj:3.0.0")
-    implementation("org.asciidoctor:asciidoctorj-diagram:3.0.1")
+    implementation("org.asciidoctor:asciidoctorj-diagram:2.3.2")
 
     // Google
     implementation("com.google.guava:guava:33.4.8-jre")
