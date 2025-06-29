b8d11da29ed8ea53662f1aeddbeb06467ae03f3c
Werner Fouché <werner.fouche@gmail.com>
2025-06-29 11:54:06 +0200

    Use revealjs@5.2.0 to build the presentation

--- doc/presentation/build.cmd
+++ doc/presentation/build.cmd
@@ -3,4 +3,4 @@ jbang run asciidoctorj-cli@wfouche ^
     -r asciidoctor-diagram ^
     presentation.adoc ^
     -b revealjs ^
-    -a revealjsdir=https://cdn.jsdelivr.net/npm/reveal.js@4.5.0
+    -a revealjsdir=https://cdn.jsdelivr.net/npm/reveal.js@5.2.0
