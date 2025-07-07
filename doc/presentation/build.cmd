781ef5862a5c2bbd2801ea02d0c8679b4cb08567
Werner Fouché <werner.fouche@gmail.com>
2025-07-07 19:53:21 +0200

    Asciidoctorj command adds revealjsdir if required

--- doc/presentation/build.cmd
+++ doc/presentation/build.cmd
@@ -2,6 +2,4 @@ jbang run asciidoctorj@wfouche ^
     -r asciidoctor-revealjs ^
     -r asciidoctor-diagram ^
     presentation.adoc ^
-    -b revealjs ^
-    -a revealjsdir=https://cdn.jsdelivr.net/npm/reveal.js@5.2.0
-    
+    -b revealjs
--- doc/presentation/build.sh
+++ doc/presentation/build.sh
@@ -3,5 +3,4 @@ jbang run asciidoctorj@wfouche \
     -r asciidoctor-revealjs \
     -r asciidoctor-diagram \
     presentation.adoc \
-    -b revealjs \
-    -a revealjsdir=https://cdn.jsdelivr.net/npm/reveal.js@5.2.0
+    -b revealjs
