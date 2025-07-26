21b09cbe1919a9e8f167e8d57ac14d4e0975183a
Werner Fouché <werner.fouche@gmail.com>
2025-07-26 22:32:23 +0200

    Fixed version number

--- scripts/AsciiArt.kt
+++ scripts/AsciiArt.kt
@@ -1,11 +1,12 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
+
 //DEPS io.leego:banana:2.1.0
 
 import io.leego.banana.BananaUtils
 import io.leego.banana.Font
 
 fun main() {
-   val text = "Tulip 2.0"
+   val text = "Tulip 2.1"
    val ascii = BananaUtils.bananaify(text, Font.STANDARD)
    println(ascii)
 }
