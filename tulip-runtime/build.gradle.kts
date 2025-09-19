7172f5e9aa6b60121629380a5e93ca55ce985705
Werner Fouché <werner.fouche@gmail.com>
2025-09-19 19:45:28 +0200

    Updated Maven Publishing information (#169)

--- tulip-runtime/build.gradle.kts
+++ tulip-runtime/build.gradle.kts
@@ -153,7 +153,6 @@ publishing {
                     developer {
                         id.set("wfouche")
                         name.set("Werner Fouché")
-                        email.set("werner.fouche@gmail.com")
                     }
                 }
                 scm {
