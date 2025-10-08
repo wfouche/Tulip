bc7ea171c49a5695559ed5bd27ae08fa2f2857f6
Werner Fouché <werner.fouche@gmail.com>
2025-10-08 10:07:48 +0200

    Measure how accurate nanosecond measurements are

--- scripts/nanos.java
+++ scripts/nanos.java
@@ -4,13 +4,14 @@ class nanos {
 
         // ....
         for (int j = 0; j < 10; j++) {
+            long t0 = System.nanoTime();
+            long t1 = t0;
             for (int i = 0; i < duration.length; i++) {
-                long t0 = System.nanoTime();
-                long t1 = System.nanoTime();
                 while (t1 == t0) {
                     t1 = System.nanoTime();
                 }
                 duration[i] = t1-t0;
+                t0 = t1;
             }
         }
 
