cf1eac41a2f92d579e20d4a671d16a23e0878199
Werner Fouché <werner.fouche@gmail.com>
2025-10-23 09:57:51 +0200

    Dependency updates

--- tulip-runtime/build.gradle.kts
+++ tulip-runtime/build.gradle.kts
@@ -83,7 +83,7 @@ dependencies {
     // - Spring Boot
 
     // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-web
-    implementation("org.springframework.boot:spring-boot-starter-web:3.5.7")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.5.6")
 
     // - SLF4J and Logback
 
