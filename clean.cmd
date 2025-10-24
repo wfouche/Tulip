337f1963a1922215f2c726d96ad3d5c1de0575cf
Werner Fouché <werner.fouche@gmail.com>
2025-10-24 07:58:23 +0200

    Delete tulip-runtime files in .m2 cache

--- clean.cmd
+++ clean.cmd
@@ -5,3 +5,7 @@ timeout 5
 rd/q/s .gradle
 timeout 5
 REM rd/q/s .idea
+
+md %USERPROFILE%\.m2
+dir /s %USERPROFILE%\.m2\tulip-runtime*.jar
+rd/q/s %USERPROFILE%\.m2\repository\io\github\wfouche\tulip\tulip-runtime
--- clean.sh
+++ clean.sh
@@ -11,3 +11,9 @@ popd || exit
 pushd ./tulip-main || exit
 source clean.sh
 popd || exit
+
+# Clean
+mkdir -p ~/.m2
+find ~/.m2 -name 'tulip-runtime*.jar' -print | sort
+rm -f -r ~/.m2/repository/io/github/wfouche/tulip/tulip-runtime
+
