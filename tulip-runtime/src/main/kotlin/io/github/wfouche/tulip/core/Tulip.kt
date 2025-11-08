3f0983013e1d993a8deb5ef6dd7435e14d1af8cb
Werner Fouché <werner.fouche@gmail.com>
2025-11-08 13:02:46 +0200

    Refactored data class Duration to file Duration.kt

--- tulip-runtime/src/main/kotlin/io/github/wfouche/tulip/core/Tulip.kt
+++ tulip-runtime/src/main/kotlin/io/github/wfouche/tulip/core/Tulip.kt
@@ -220,34 +220,6 @@ private data class Action(
 
 /*-------------------------------------------------------------------------*/
 
-private data class Duration(
-
-    // Start-up only executed once per TestCase.
-    //
-    val startupDurationUnits: Long = 0,
-
-    // Warm-up phase executed once per TestCase.
-    //
-    val warmupDurationUnits: Long = 0,
-
-    // Main duration in minutes.
-    // The results from this period are reported.
-    //
-    // The main phase executed 'repeatCount' every iteration of TestCase.
-    //
-    val mainDurationUnits: Long = 0,
-    val mainDurationRepeatCount: Int = 1,
-    val timeUnit: TimeUnit = TimeUnit.SECONDS,
-
-    // ------------------------------
-
-    val startupDurationMillis: Long = timeUnit.toMillis(startupDurationUnits),
-    val warmupDurationMillis: Long = timeUnit.toMillis(warmupDurationUnits),
-    val mainDurationMillis: Long = timeUnit.toMillis(mainDurationUnits),
-)
-
-/*-------------------------------------------------------------------------*/
-
 private data class TestProfile(
     val enabled: Boolean = true,
     val saveStats: Boolean = true,
