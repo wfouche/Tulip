167e9b6bb56205cd60abafa59fbb721b513363c9
Werner Fouché <werner.fouche@gmail.com>
2025-11-09 21:10:21 +0200

    Set python.console.encoding to UTF-8 (#285)
    * Set python.console.encoding to UTF-8
    
    * Added Windows OS check

--- tulip-runtime/src/main/kotlin/io/github/wfouche/tulip/core/Tulip.kt
+++ tulip-runtime/src/main/kotlin/io/github/wfouche/tulip/core/Tulip.kt
@@ -32,6 +32,7 @@ import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonPrimitive
 import org.HdrHistogram.Histogram
 import org.HdrHistogram.IntCountsHistogram
+import org.apache.commons.lang3.SystemUtils
 
 /*-------------------------------------------------------------------------*/
 
@@ -779,9 +780,13 @@ private fun runTest(
 
 private fun initTulip() {
     Console.put(TulipApi.getVersionBanner())
-    val tulip = String(Character.toChars(0x0001F337))
+    var tulip = " " + String(Character.toChars(0x0001F337))
+    if (SystemUtils.IS_OS_WINDOWS) {
+        tulip = ""
+    }
     Console.put(
-        tulip + " Tulip $VERSION (Java: ${System.getProperty("java.vendor")} ${System.getProperty("java.runtime.version")}, Kotlin: ${KotlinVersion.CURRENT})"
+        "Tulip $VERSION (Java: ${System.getProperty("java.vendor")} ${System.getProperty("java.runtime.version")}, Kotlin: ${KotlinVersion.CURRENT})" +
+            tulip
     )
 }
 
