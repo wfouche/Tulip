0838661e9c1146192d9fc7c6d8bc3061c33b269c
Werner Fouché <werner.fouche@gmail.com>
2025-11-19 17:47:16 +0200

    Update Gradle to 9.2.1 (#291)

--- upgrade_gradle.sh
+++ upgrade_gradle.sh
@@ -10,6 +10,6 @@
 #
 # https://github.com/Kotlin/kotlinx.serialization - 1.7.2 requries Kotlin 2.0.20
 #
-GRADLE_VERSION=9.2.0
+GRADLE_VERSION=9.2.1
 
 ./gradlew --no-daemon wrapper --gradle-version $GRADLE_VERSION && ./gradlew wrapper
