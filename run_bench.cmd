538329a7a3d7583fdd9eb989dddef57ab6891406
Werner Fouché <werner.fouche@gmail.com>
2025-12-05 11:37:02 +0200

    Added "chcp 65001" to CMD benchmark scripts

--- run_bench.cmd
+++ run_bench.cmd
@@ -1,1 +1,2 @@
+chcp 65001 > nul
 .\build_and_run.cmd --args="--config benchmark_config.jsonc"
--- run_bench_test.cmd
+++ run_bench_test.cmd
@@ -1,3 +1,4 @@
+chcp 65001 > nul
 REM call .\gradlew :tulip-app:run --args="--config benchmark_config_test.json"
 REM call .\test_report.cmd
 .\build_and_run.cmd --args="--config benchmark_config_test.jsonc"
