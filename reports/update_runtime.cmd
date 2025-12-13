2101fae7acab3395ecbbac3cd289d27514a470e5
Werner Fouché <werner.fouche@gmail.com>
2025-12-13 19:16:53 +0200

    Jython console UTF-8 enabled

--- reports/update_runtime.cmd
+++ reports/update_runtime.cmd
@@ -1,7 +1,7 @@
 REM python3 jython/jython-cli.py report.py
-call jbang run python_jvm_tulip.java --keep-java report.py
+call jbang run --runtime-option -Dstdout.encoding=UTF-8 --runtime-option -Dpython.console.encoding=UTF-8 python_jvm_tulip.java --keep-java report.py
 REM python3 jython/jython-cli.py report2.py
-call jbang run python_jvm_tulip.java --keep-java report2.py
+call jbang run --runtime-option -Dstdout.encoding=UTF-8 --runtime-option -Dpython.console.encoding=UTF-8 python_jvm_tulip.java --keep-java report2.py
 
 REM copy package.txt+report_py.java report.txt
 REM copy report.txt report_py.java
