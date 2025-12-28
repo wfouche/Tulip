bc8d8c20e7f42abf4bc6217fc66f2f6bfadd4c0a
Werner Fouché <werner.fouche@gmail.com>
2025-12-28 18:39:20 +0200

    Updated report runtime (#324)

--- reports/report.py
+++ reports/report.py
@@ -17,8 +17,6 @@ except:
 # requires-jython = "2.7.4"
 # requires-java = "21"
 # dependencies = [
-#   "com.google.code.gson:gson:2.13.1",
-#   "org.hdrhistogram:HdrHistogram:2.2.2",
 #   "io.github.wfouche.tulip:tulip-runtime:2.1.14"
 # ]
 # ///
