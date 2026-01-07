87b67ab93c5dbad1d25d1ee7025fa8493f52bd3a
Werner Fouché <werner.fouche@gmail.com>
2026-01-07 22:54:15 +0200

    Update Logback to 1.5.24

--- tulip-runtime/build.gradle.kts
+++ tulip-runtime/build.gradle.kts
@@ -90,10 +90,10 @@ dependencies {
     implementation("org.slf4j:slf4j-api:2.0.17")
 
     // https://mvnrepository.com/artifact/ch.qos.logback/logback-core
-    implementation("ch.qos.logback:logback-core:1.5.23")
+    implementation("ch.qos.logback:logback-core:1.5.24")
 
     // https://mvnrepository.com/artifact/ch.qos.logback/logback-classic
-    implementation("ch.qos.logback:logback-classic:1.5.23")
+    implementation("ch.qos.logback:logback-classic:1.5.24")
 
     // - Apache HttpClient
 
