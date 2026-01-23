eebd1f849fd0f69291cbd6af95d337e45ff33e51
Werner Fouché <werner.fouche@gmail.com>
2026-01-23 19:02:27 +0200

    Return 201 for created POSTs

--- scripts/JavalinServer.java
+++ scripts/JavalinServer.java
@@ -9,16 +9,17 @@ import java.lang.System;
 public class JavalinServer {
     public static void main(String[] args) {
         var app = Javalin.create(config -> config.useVirtualThreads=true)
-            .get("/", ctx -> ctx.result("{\"code\": \"OK\"}").contentType("application/json"))
+            .get("/", ctx -> {ctx.status(200); ctx.result("{\"code\": \"OK\"}").contentType("application/json");} )
             .get("/exit", ctx -> {System.out.println("Exit."); System.exit(0); })
-            .post("/posts/", ctx -> ctx.result("{\"code\": \"OK\"}").contentType("application/json") )
-            .get("/posts/{id}", ctx -> ctx.result("{\"code\": \"OK\"}").contentType("application/json") )
-            .put("/posts/{id}", ctx -> ctx.result("{\"code\": \"OK\"}").contentType("application/json") )
-            .delete("/posts/{id}", ctx -> ctx.result("{\"code\": \"OK\"}").contentType("application/json") )
-            .get("/comments/{id}", ctx -> ctx.result("{\"code\": \"OK\"}").contentType("application/json") )
-            .get("/albums/{id}", ctx -> ctx.result("{\"code\": \"OK\"}").contentType("application/json") )
-            .get("/photos/{id}", ctx -> ctx.result("{\"code\": \"OK\"}").contentType("application/json") )
-            .get("/todos/{id}", ctx -> ctx.result("{\"code\": \"OK\"}").contentType("application/json") )
+            .post("/posts", ctx -> {ctx.status(201); ctx.result("{\"code\": \"OK\"}").contentType("application/json");} )
+            .get("/posts", ctx -> {ctx.status(200); ctx.result("{\"code\": \"OK\"}").contentType("application/json");} )
+            .get("/posts/{id}", ctx -> {ctx.status(200); ctx.result("{\"code\": \"OK\"}").contentType("application/json");} )
+            .put("/posts/{id}", ctx -> {ctx.status(200); ctx.result("{\"code\": \"OK\"}").contentType("application/json");} )
+            .delete("/posts/{id}", ctx -> {ctx.status(200); ctx.result("{\"code\": \"OK\"}").contentType("application/json");} )
+            .get("/comments/{id}", ctx -> {ctx.status(200); ctx.result("{\"code\": \"OK\"}").contentType("application/json");} )
+            .get("/albums/{id}", ctx -> {ctx.status(200); ctx.result("{\"code\": \"OK\"}").contentType("application/json");} )
+            .get("/photos/{id}", ctx -> {ctx.status(200); ctx.result("{\"code\": \"OK\"}").contentType("application/json");} )
+            .get("/todos/{id}", ctx -> {ctx.status(200); ctx.result("{\"code\": \"OK\"}").contentType("application/json");} )
             // curl -X GET http://localhost:7070:/rendezvous/ -H 'Content-Type: application/json' -d '{"login":"my_login","password":"my_password"}'
             .get("/rendezvous", ctx -> {
                 System.out.println(ctx.body());
