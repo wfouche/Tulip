5c33b6e4234d1cf29440027f08f579945605cbea
Werner Fouché <werner.fouche@gmail.com>
2026-01-25 10:09:39 +0200

    Virtual thread support requires JDK 21+

--- buildSrc/src/main/kotlin/buildlogic.kotlin-common-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/buildlogic.kotlin-common-conventions.gradle.kts
@@ -29,13 +29,13 @@ dependencies {
 // Apply a specific Java toolchain to ease working on different environments.
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
+        languageVersion = JavaLanguageVersion.of(21)
         vendor.set(JvmVendorSpec.ADOPTIUM)
     }
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 tasks.named<Test>("test") {
