9cad942f6f0993a109a474d567e787cdf92b8431
Werner Fouché <werner.fouche@gmail.com>
2026-01-30 22:21:50 +0200

    Next development cycle

--- tulip-runtime/src/main/java/io/github/wfouche/tulip/api/TulipApi.java
+++ tulip-runtime/src/main/java/io/github/wfouche/tulip/api/TulipApi.java
@@ -31,7 +31,7 @@ public class TulipApi implements Callable<Integer> {
     TulipApi() {}
 
     /** The version string of the Tulip API. */
-    public static final String VERSION = "2.2.0";
+    public static final String VERSION = "2.2.1";
 
     /** The number of unique actions available in the benchmarking process. */
     public static final int NUM_ACTIONS = 101;
