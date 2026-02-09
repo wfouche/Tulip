8077cc95366abf413b4913e8ae671edf9552bd63
Werner Fouché <werner.fouche@gmail.com>
2026-02-09 09:21:44 +0200

    Next development cycle

--- buildMavenCentralArtifacts.sh
+++ buildMavenCentralArtifacts.sh
@@ -3,7 +3,7 @@ source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk env
 
 # Tulip version
-export version="2.2.2"
+export version="2.2.3"
 
 # Remove staging folder
 rm -f -r tulip-runtime/build/staging-deploy
