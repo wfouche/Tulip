3ece1bacca51e33bf1c74c018eeaec55fc321cee
Werner Fouché <werner.fouche@gmail.com>
2026-02-24 21:05:01 +0200

    Use Javalin 7.0.0

--- scripts/JavalinServer.java
+++ scripts/JavalinServer.java
@@ -1,32 +1,62 @@
-///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS io.javalin:javalin:6.7.0
-//DEPS org.slf4j:slf4j-simple:2.0.17
-//JAVA 21
-//RUNTIME_OPTIONS -Xmx2g -Xms2g -XX:+UseG1GC
+/// usr/bin/env jbang "$0" "$@" ; exit $?
+//DEPS io.javalin:javalin:7.0.0
+//DEPS org.slf4j:slf4j-simple:2.0.12
 
-// https://javalin.io/
 import io.javalin.Javalin;
-import java.lang.System;
 
 public class JavalinServer {
     public static void main(String[] args) {
-        var app = Javalin.create(config -> config.useVirtualThreads=true)
-            .get("/", ctx -> {ctx.status(200); ctx.result("{\"code\": \"OK\"}").contentType("application/json");} )
-            .get("/exit", ctx -> {System.out.println("Exit."); System.exit(0); })
-            .post("/posts", ctx -> {ctx.status(201); ctx.result("{\"code\": \"OK\"}").contentType("application/json");} )
-            .get("/posts", ctx -> {ctx.status(200); ctx.result("{\"code\": \"OK\"}").contentType("application/json");} )
-            .get("/posts/{id}", ctx -> {ctx.status(200); ctx.result("{\"code\": \"OK\"}").contentType("application/json");} )
-            .put("/posts/{id}", ctx -> {ctx.status(200); ctx.result("{\"code\": \"OK\"}").contentType("application/json");} )
-            .delete("/posts/{id}", ctx -> {ctx.status(200); ctx.result("{\"code\": \"OK\"}").contentType("application/json");} )
-            .get("/comments/{id}", ctx -> {ctx.status(200); ctx.result("{\"code\": \"OK\"}").contentType("application/json");} )
-            .get("/albums/{id}", ctx -> {ctx.status(200); ctx.result("{\"code\": \"OK\"}").contentType("application/json");} )
-            .get("/photos/{id}", ctx -> {ctx.status(200); ctx.result("{\"code\": \"OK\"}").contentType("application/json");} )
-            .get("/todos/{id}", ctx -> {ctx.status(200); ctx.result("{\"code\": \"OK\"}").contentType("application/json");} )
-            // curl -X GET http://localhost:7070:/rendezvous/ -H 'Content-Type: application/json' -d '{"login":"my_login","password":"my_password"}'
-            .get("/rendezvous", ctx -> {
+        var app = Javalin.create(config -> {
+            config.concurrency.useVirtualThreads = true;
+
+            config.routes.get("/", ctx -> {
+                ctx.status(200);
+                ctx.result("{\"code\": \"OK\"}").contentType("application/json");
+            });
+            config.routes.post("/posts", ctx -> {
+                ctx.status(201);
+                ctx.result("{\"code\": \"OK\"}").contentType("application/json");
+            });
+            config.routes.get("/posts", ctx -> {
+                ctx.status(200);
+                ctx.result("{\"code\": \"OK\"}").contentType("application/json");
+            });
+            config.routes.get("/posts/{id}", ctx -> {
+                ctx.status(200);
+                ctx.result("{\"code\": \"OK\"}").contentType("application/json");
+            });
+            config.routes.put("/posts/{id}", ctx -> {
+                ctx.status(200);
+                ctx.result("{\"code\": \"OK\"}").contentType("application/json");
+            });
+            config.routes.delete("/posts/{id}", ctx -> {
+                ctx.status(200);
+                ctx.result("{\"code\": \"OK\"}").contentType("application/json");
+            });
+            config.routes.get("/comments/{id}", ctx -> {
+                ctx.status(200);
+                ctx.result("{\"code\": \"OK\"}").contentType("application/json");
+            });
+            config.routes.get("/albums/{id}", ctx -> {
+                ctx.status(200);
+                ctx.result("{\"code\": \"OK\"}").contentType("application/json");
+            });
+            config.routes.get("/photos/{id}", ctx -> {
+                ctx.status(200);
+                ctx.result("{\"code\": \"OK\"}").contentType("application/json");
+            });
+            config.routes.get("/todos/{id}", ctx -> {
+                ctx.status(200);
+                ctx.result("{\"code\": \"OK\"}").contentType("application/json");
+            });
+            config.routes.get("/exit", ctx -> {
+                System.out.println("Exit.");
+                System.exit(0);
+            });
+            config.routes.get("/rendezvous", ctx -> {
                 System.out.println(ctx.body());
                 ctx.result("{\"code\": \"OK\"}").contentType("application/json");
-            } )
-            .start(7070);
+            });
+        }).start(7070);
     }
 }
