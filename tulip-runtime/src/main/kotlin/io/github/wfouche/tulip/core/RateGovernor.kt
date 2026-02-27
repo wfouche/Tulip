7943c5c716f8b6940d3ac00870eca8b55f3f0ea2
Werner Fouché <werner.fouche@gmail.com>
2026-02-27 19:57:39 +0200

    Ktlint formatting updates for RateGovernor.kt

--- tulip-runtime/src/main/kotlin/io/github/wfouche/tulip/core/RateGovernor.kt
+++ tulip-runtime/src/main/kotlin/io/github/wfouche/tulip/core/RateGovernor.kt
@@ -6,15 +6,15 @@ class RateGovernor(
     private val averageRate: Double,
     private val timeMillisStart: Long = TimeUnit.NANOSECONDS.toMillis(System.nanoTime()),
 ) {
-
     private var count: Long = 0
 
     fun pace() {
         count += 1
         val deltaMs: Long =
-            (timeMillisStart + count * (1000 / averageRate) -
-                    TimeUnit.NANOSECONDS.toMillis(System.nanoTime()))
-                .toLong()
+            (
+                timeMillisStart + count * (1000 / averageRate) -
+                    TimeUnit.NANOSECONDS.toMillis(System.nanoTime())
+            ).toLong()
         if (deltaMs > 0) Thread.sleep(deltaMs)
     }
 }
