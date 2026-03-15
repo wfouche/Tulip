43eec650138943f34513ff2761e91414035931c2
Werner Fouché <werner.fouche@gmail.com>
2026-03-15 20:28:16 +0200

    Updated relocation pom.xml (#436)

--- scripts/maven/sign_pom.cmd
+++ scripts/maven/sign_pom.cmd
@@ -1,5 +1,5 @@
 mvn gpg:sign-and-deploy-file ^
   -Durl=https://central.sonatype.com/ ^
   -DrepositoryId=central ^
-  -DpomFile=tulip.xml ^
-  -Dfile=tulip.xml
+  -DpomFile=pom.xml ^
+  -Dfile=pom.xml
