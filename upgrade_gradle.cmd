98bacc80ecae849a19fb204103edc6a46f101418
Werner Fouché <werner.fouche@gmail.com>
2026-03-20 07:45:06 +0200

    Use Gradle 9.4.1 (#449)
    * Use Gradle 9.4.1
    
    * Updated gradlew file

--- upgrade_gradle.cmd
+++ upgrade_gradle.cmd
@@ -1,3 +1,3 @@
 REM .\gradlew.bat wrapper --gradle-version latest
-call .\gradlew.bat --no-daemon wrapper --gradle-version 9.4.0
+call .\gradlew.bat --no-daemon wrapper --gradle-version 9.4.1
 call .\gradlew.bat --no-daemon wrapper
