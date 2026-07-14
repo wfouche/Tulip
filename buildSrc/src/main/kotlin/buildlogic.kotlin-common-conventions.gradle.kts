11a4b9aba1386558f1277a780293279aaba90120
Werner Fouché <werner.fouche@gmail.com>
2026-07-14 14:31:13 +0200

    Update dependencies (#540)

--- buildSrc/src/main/kotlin/buildlogic.kotlin-common-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/buildlogic.kotlin-common-conventions.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
     }
 
     // Use JUnit Jupiter for testing.
-    testImplementation("org.junit.jupiter:junit-jupiter:6.1.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:6.1.2")
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
