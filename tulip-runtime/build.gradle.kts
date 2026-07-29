8d23219f5526d0ad5e926370c4d4217d25b0f135
Werner Fouché <werner.fouche@gmail.com>
2026-07-29 21:57:38 +0200

    Update logback (#545)

--- tulip-runtime/build.gradle.kts
+++ tulip-runtime/build.gradle.kts
@@ -46,7 +46,7 @@ java {
 
 val gsonVersion = "2.14.0"
 val slf4jVersion = "2.0.18"
-val logbackVersion = "1.6.0"
+val logbackVersion = "1.6.1"
 val springBootVersion = "3.5.16"
 val picocliVersion = "4.7.7"
 val javalinVersion = "7.2.2"
